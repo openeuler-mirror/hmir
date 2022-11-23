fd265f0a55614db5686c7391fb2579df7e9ccf32
ckxu <dev17302@linx-info.com>
2022-11-23 20:26:10 +0800

    添加获取ovs网桥port信息的能力

--- src/net.rs
+++ src/net.rs
@@ -11,6 +11,8 @@ use std::os::unix::net::UnixStream;
 use std::io::prelude::*;
 use std::net::Shutdown;
 use std::string::FromUtf8Error;
+use uuid::Uuid;
+use serde_json::json;
 
 fn u8v_to_string(v : Vec<u8>) -> Result<String, FromUtf8Error>{
     String::from_utf8(v)
@@ -23,7 +25,7 @@ pub struct OvsPort{
     pub mode : OvsPortMode
 }
 
-#[derive(Debug, Clone,Serialize)]
+#[derive(Debug, Clone, Serialize)]
 pub enum OvsPortMode{
     Access(u16),
     Trunk(Vec<u16>)
@@ -63,6 +65,8 @@ pub enum OvsErrorType{
     ConnectionError,
     InvalidResponse,
     InvalidResponseJson,
+    UnexpectedResponse,
+    InconsistentInstruction,
     QueryError
 }
 
@@ -107,12 +111,14 @@ impl Display for OvsError{
 
 // ovs interface
 pub struct OvsClient{
+    transaction_id : i32,
     target : String
 }
 
 impl OvsClient{
     pub fn new() -> Result<OvsClient, OvsError>{
         Ok(OvsClient{
+            transaction_id : 0,
             target : String::from("/var/run/openvswitch/db.sock")
         })
     }
@@ -129,18 +135,259 @@ impl OvsClient{
     }
 
     fn get_ports(&mut self) -> Result<Vec<OvsPort>, OvsError>{
-        todo!() 
+        let query = serde_json::from_str(
+            "{\"method\": \"transact\",\"params\":[\"Open_vSwitch\",{\"op\":\"select\",\"table\":\"Port\",\"where\":[]}],\"id\":0}"
+        ).unwrap();
+        
+        let resp = self.send_msg(query);
+        let mut ports:Vec<OvsPort> = Vec::new();
+        
+        match resp{
+            Err(e) => return Err(e),
+            Ok(data) => {
+                for p in data["result"][0]["rows"].as_array().unwrap(){
+                    
+                    let name: &str= p["name"].as_str().ok_or(
+                        OvsError::new(
+                            OvsErrorType::UnexpectedResponse,
+                            "key ['name'] is not found in response data"
+                        )
+                    )?;
+                    
+                    let uuid: &str= p["_uuid"][1].as_str().ok_or(
+                        OvsError::new(
+                            OvsErrorType::UnexpectedResponse,
+                            "key [_'uuid'][1] is not found in response data"
+                        )
+                    )?;
+                    
+                    if p["trunks"][1].as_array().unwrap().len() > 0{
+                        let mut trunk_vlans : Vec<u16> = Vec::new();
+                        for vlan in p["trunks"][1].as_array().unwrap(){
+                            trunk_vlans.push(vlan.as_u64().unwrap() as u16);
+                        }
+                        
+                        ports.push(OvsPort::new(
+                            name,
+                            uuid,
+                            &OvsPortMode::Trunk(trunk_vlans.clone())
+                        ));
+                        continue;
+                    }
+                    
+                    match p["tag"].as_u64(){
+                        None => {},
+                        Some(vlan) => {
+                            ports.push(OvsPort::new(
+                                name,
+                                uuid,
+                                &OvsPortMode::Access(vlan as u16)
+                            ));
+                            continue;
+                        }
+                    }
+                    
+                    ports.push(OvsPort::new(
+                        name,
+                        uuid,
+                        &OvsPortMode::Trunk(Vec::new())
+                    ));
+                    
+                }
+            }
+        }
+        
+        Ok(ports)
     }
 
     fn get_bridges(&mut self) -> Result<Vec<OvsBridge>, OvsError>{
-        todo!() 
+        let query = serde_json::from_str(
+            "{\"method\": \"transact\",\"params\":[\"Open_vSwitch\",{\"op\":\"select\",\"table\":\"Bridge\",\"where\":[]}],\"id\":0}"
+        ).unwrap();
+        
+        let resp = self.send_msg(query);
+        let mut bridges:Vec<OvsBridge> = Vec::new();
+        
+        match resp{
+            Err(e) => return Err(e),
+            Ok(data) => {
+                let ports = self.get_ports()?;
+                
+                for br in data["result"][0]["rows"].as_array().unwrap(){
+                    let name: &str= br["name"].as_str().ok_or(
+                        OvsError::new(
+                            OvsErrorType::UnexpectedResponse,
+                            "key ['name'] is not found in response data"
+                        )
+                    )?;
+                    
+                    let uuid: &str= br["_uuid"][1].as_str().ok_or(
+                        OvsError::new(
+                            OvsErrorType::UnexpectedResponse,
+                            "key [_'uuid'][1] is not found in response data"
+                        )
+                    )?;
+                    
+                    let mut b = OvsBridge::new(
+                        name,
+                        uuid
+                    );
+                    
+                    match br["ports"][1].as_array(){
+                        None=>{},
+                        Some(br_ports)=>{
+                            for bp in br_ports{
+                                for p in &ports{
+                                    if bp[1]== p.uuid{
+                                        b.ports.push(p.clone());
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                    bridges.push(b);
+                }
+            }
+        }
+        
+        Ok(bridges)
     }
 
     fn add_port(&mut self, bridge_name:&str, port_name: &str, port_mode: &OvsPortMode) -> Result<serde_json::Value, OvsError>{
-        todo!() 
+        let ports = self.get_ports()?;
+        let bridges = self.get_bridges()?;
+        
+        for p in ports{
+            if p.name == port_name{
+                return Err(
+                        OvsError::new(
+                        OvsErrorType::InconsistentInstruction,
+                        &format!("Interface already exist in ovsdb. ({})", port_name)
+                    )
+                )
+            }
+        }
+        
+        //let mut target_bridge: Option<&OvsBridge> = None;
+        //let target_bridge = self.get_bridge(bridge_name);
+        let mut port_list:Vec<Vec<String>> = Vec::new();
+        
+        let target_bridge = match self.get_bridge(bridge_name){
+            None=>{
+                return Err(
+                        OvsError::new(
+                        OvsErrorType::InconsistentInstruction,
+                        &format!("Bridge is not found. ({})", bridge_name)
+                    )
+                )
+            },
+            Some(b) =>{
+                println!("{}", serde_json::to_string(&b).unwrap());
+                for p in &b.ports{
+                    port_list.push(vec!("uuid".to_string(), b.uuid.clone()));
+                }
+                println!("{}", serde_json::to_string(&port_list).unwrap());
+                b
+            }
+        };
+        
+        let interface_tmp_uuid = format!("row{}", Uuid::new_v4()).replace("-", "_");
+        let port_tmp_uuid = format!("row{}", Uuid::new_v4()).replace("-", "_");
+        
+        port_list.push(vec!("named-uuid".to_string(), port_tmp_uuid.clone()));
+        
+        let mut row_json = json!({});
+        match port_mode{
+            &OvsPortMode::Access(vlan)=>{
+                row_json = json!({
+                    "name" : port_name,
+                    "interfaces":[
+                        "named-uuid",
+                        interface_tmp_uuid
+                    ],
+                    "tag" : vlan
+                });
+            },
+            &OvsPortMode::Trunk(ref vlans)=>{
+                let mut vlan_list:Vec<u64> = Vec::new();
+                for vlan in vlans{
+                    vlan_list.push(vlan.clone().into());
+                }
+                let trunks = json!(["set",vlan_list]);
+                row_json = json!({
+                    "name" : port_name,
+                    "interfaces":[
+                        "named-uuid",
+                        interface_tmp_uuid
+                    ],
+                    "trunks" : trunks
+                });
+            }
+        }
+        
+        let mut query = json!({
+            "method":"transact",
+            "params":[
+                "Open_vSwitch",
+                {
+                    "uuid-name" : interface_tmp_uuid,
+                    "op" : "insert",
+                    "table" : "Interface",
+                    "row": {
+                        "name":port_name,
+                        "type":""
+                    }
+                },
+                {
+                    "uuid-name": port_tmp_uuid,
+                    "op" : "insert",
+                    "table" : "Port",
+                    "row":row_json
+                },
+                {
+                    "where": [
+                        [
+                            "_uuid",
+                            "==",
+                            [
+                                "uuid",
+                                target_bridge.uuid
+                            ]
+                        ]
+                    ],
+                    "row": {
+                        "ports": [
+                            "set",
+                            port_list
+                        ]
+                    },
+                    "op": "update",
+                    "table": "Bridge"
+                }
+            ],
+            "id":self.transaction_id
+        });
+        
+        println!("{}", query);
+        self.send_msg(query)
+    }
+
+    fn get_bridge(&mut self, bridge_name:&str) -> Option<OvsBridge>{
+        let bridges = self.get_bridges().unwrap();
+        
+        for i in  0..bridges.len(){
+            if bridges[i].name == bridge_name{
+                return Some(bridges[i].clone());
+            }
+        }
+        
+        None
     }
 
     fn send_msg(&mut self, msg : serde_json::Value) -> Result<serde_json::Value, OvsError>{
+        self.transaction_id += 1;
         let mut socket = match UnixStream::connect(&self.target){
             Ok(con) => con,
             Err(e) =>return  Err(
@@ -207,6 +454,16 @@ struct RetInfo {
     message: String,
 }
 
+#[derive(Clone, Serialize)]
+struct RetPort{
+    ovs_ports: Vec<OvsPort>,
+}
+
+#[derive(Clone, Serialize)]
+struct RetBridge{
+    ovs_bridges: Vec<OvsBridge>,
+}
+
 pub fn check_connection() -> std::string::String{
     let ovsc = OvsClient::new();
     match ovsc{
@@ -251,10 +508,10 @@ pub fn get_ports() -> std::string::String{
             match ports{
                 Ok(ports) =>{
                     println!("number of port : {0}", ports.len());
-                    let mut ret_message  =  "".to_string();
-                    for port in ports{
-                        //ret_message = serde_json::to_string(&port).unwrap();
-                    }
+                    let ret_info = RetPort {
+                        ovs_ports: ports,
+                    };
+                    let ret_message = serde_json::to_string(&ret_info).unwrap();
                     ret_message    
                 },
                 Err(e) => {
@@ -270,7 +527,36 @@ pub fn get_ports() -> std::string::String{
 }
 
 pub fn get_bridges() -> std::string::String {
-    todo!()
+    let ovsc = OvsClient::new();
+    match ovsc{
+        Err(e) => {
+            let ret_info = RetInfo {
+                message: e.error_detail.clone(),
+            };
+            let ret_message = serde_json::to_string(&ret_info).unwrap();
+            ret_message
+        },
+        Ok(mut c)=>{
+            let bridges = c.get_bridges();
+            match bridges{
+                Ok(bridges) =>{
+                    println!("number of bridge: {0}", bridges.len());
+                    let ret_info = RetBridge {
+                        ovs_bridges: bridges,
+                    };
+                    let ret_message = serde_json::to_string(&ret_info).unwrap();
+                    ret_message    
+                },
+                Err(e) => {
+                    let ret_info = RetInfo {
+                        message: e.error_detail.clone(),
+                    };
+                    let ret_message = serde_json::to_string(&ret_info).unwrap();
+                    ret_message
+                }
+            }
+        }
+    }
 }
 
 pub fn add_port() -> std::string::String {
