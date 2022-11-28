099898443900e375c5dbe8bfb3e306f3b0af1e2d
ckxu <dev17302@linx-info.com>
2022-11-28 14:43:03 +0800

    网络管理模块： ovs添加帮助信息

--- src/net/ovs.rs
+++ src/net/ovs.rs
@@ -1,3 +1,80 @@
+//! ovs管理模块
+//! 
+//! 支持以下的格式
+//! - ovs-check-connection: 查询ovsdb连接状态
+//! 请求格式：
+//!  ```
+//! { 
+//!     "jsonrpc":"2.0", 
+//!     "id":1, 
+//!     "method":"ovs-check-connection" 
+//! }
+//!  ```
+//! 响应格式：
+//! ```
+//! {
+//!     "jsonrpc":"2.0",
+//!     "result":"{\"message\":\"Done\"}",
+//!     "id":1
+//! }
+//! ```
+//! 
+//! - ovs-get-bridges： 查询ovs网桥信息
+//! 请求格式：
+//!  ```
+//! { 
+//!     "jsonrpc":"2.0", 
+//!     "id":1, 
+//!     "method":"ovs-get-bridges" 
+//! }
+//!  ```
+//! 响应格式：
+//! ```
+//! {
+//!     "jsonrpc":"2.0",
+//!     "result":"{\"ovs_bridges\":[{\"name\":\"br-br0\",\"uuid\":\"d9879f03-fa8d-49ee-8905-61bf2e678a94\",\"ports\":[{\"name\":\"ens3\",\"uuid\":\"29bb1048-53f2-41c8-8d76-2592045312c9\",\"mode\":{\"Trunk\":[]}},{\"name\":\"br-br0\",\"uuid\":\"7a3c43b0-7cb6-47bb-9e73-b7acfbce3d78\",\"mode\":{\"Trunk\":[]}},{\"name\":\"patch-out\",\"uuid\":\"c863f5e9-e312-4694-9279-01650284d3ae\",\"mode\":{\"Trunk\":[]}}]},{\"name\":\"br-test\",\"uuid\":\"fbc4b9ea-930b-4a53-845a-68d9bf5d46e4\",\"ports\":[{\"name\":\"patch-in\",\"uuid\":\"1cc48e17-63b7-4af3-a2d2-d709f9152497\",\"mode\":{\"Trunk\":[]}},{\"name\":\"vnet1\",\"uuid\":\"715a810a-c3e4-4407-86d7-c003add84406\",\"mode\":{\"Access\":100}},{\"name\":\"br-test\",\"uuid\":\"c082c812-3a84-4104-a8d3-c93720cbd959\",\"mode\":{\"Trunk\":[]}}]}]}",
+//!    "id":1
+//! }
+//! ```
+//!  //! - ovs-get-ports： 查询ovs端口信息
+//! 请求格式：
+//!  ```
+//! { 
+//!     "jsonrpc":"2.0", 
+//!     "id":1, 
+//!     "method":"ovs-get-ports" 
+//! }
+//!  ```
+//! 响应格式：
+//! ```
+//! {
+//!     "jsonrpc":"2.0",
+//!     "result":"{\"ovs_ports\":[{\"name\":\"br-br0\",\"uuid\":\"7a3c43b0-7cb6-47bb-9e73-b7acfbce3d78\",\"mode\":{\"Trunk\":[]}},{\"name\":\"br-test\",\"uuid\":\"c082c812-3a84-4104-a8d3-c93720cbd959\",\"mode\":{\"Trunk\":[]}},{\"name\":\"ens3\",\"uuid\":\"29bb1048-53f2-41c8-8d76-2592045312c9\",\"mode\":{\"Trunk\":[]}},{\"name\":\"patch-in\",\"uuid\":\"1cc48e17-63b7-4af3-a2d2-d709f9152497\",\"mode\":{\"Trunk\":[]}},{\"name\":\"patch-out\",\"uuid\":\"c863f5e9-e312-4694-9279-01650284d3ae\",\"mode\":{\"Trunk\":[]}},{\"name\":\"vnet1\",\"uuid\":\"715a810a-c3e4-4407-86d7-c003add84406\",\"mode\":{\"Access\":100}}]}",
+//!     "id":1}
+//! ``` 
+//! 
+//! - ovs-add-port： ovs网桥中添加端口
+//! 请求格式：
+//!  ```
+//! { 
+//!     "jsonrpc":"2.0", 
+//!     "id":1, 
+//!     "method":"ovs-add-ports"
+//!      "params":{"br_name":"br-br0","port_name":"ens4", "vlan_id":"100"}
+//! }
+//!  ```
+//! 
+//! 响应格式：
+//! ```
+//! {
+//!     "jsonrpc":"2.0",
+//!      "result":"{\"error\":null,\"id\":5,\"result\":[{\"uuid\":[\"uuid\",\"43980cb5-4625-4bda-a909-063557a5fbff\"]},{\"uuid\":[\"uuid\",\"52b526c8-0624-4e59-9ee5-31e03d533485\"]},{\"count\":1}]}",
+//!     "id":1
+//! }
+//! ```
+//! 
+
+
 
 use std::collections::HashMap;
 use serde_json::json;
@@ -300,7 +377,7 @@ impl OvsClient{
             Some(b) =>{
                 println!("{}", serde_json::to_string(&b).unwrap());
                 for p in &b.ports{
-                    port_list.push(vec!("uuid".to_string(), b.uuid.clone()));
+                    port_list.push(vec!("uuid".to_string(), p.uuid.clone()));
                 }
                 println!("{}", serde_json::to_string(&port_list).unwrap());
                 b
@@ -384,7 +461,7 @@ impl OvsClient{
             "id":self.transaction_id
         });
         
-        println!("{}", query);
+        println!("query_message:{}", query);
         self.send_msg(query)
     }
 
@@ -423,7 +500,6 @@ impl OvsClient{
         let _ = socket.shutdown(Shutdown::Write);
         
         let mut s : Vec<u8> = Vec::new();
-        
         let resp_str = (
             socket.read_to_end(&mut s)
             .map_err(
@@ -436,14 +512,14 @@ impl OvsClient{
                 )
             )
         )?;
-        
+
         let resp_json: serde_json::Value = (
             serde_json::from_str(resp_str.as_str())
             .map_err(
                 |_| OvsError::new(OvsErrorType::InvalidResponseJson, "Faild to parse response data")
             )
         )?;
-        
+
         match resp_json["result"][0].as_object(){
             None => {
                 return Err(OvsError::new(OvsErrorType::InvalidResponse, "Faild to parse response data"))
@@ -457,7 +533,7 @@ impl OvsClient{
                 }
             }
         }
-        
+
         Ok(resp_json)
     }
     
@@ -574,6 +650,7 @@ pub fn add_port(info_map : HashMap<String, String>) -> std::string::String {
             let port_name = info_map.get("port_name").unwrap();
             let vlan_str = info_map.get("vlan_id").unwrap();
             let vlan_id = vlan_str.parse::<u16>().unwrap();
+            println!("br_name:{},port_name:{},vlan_id:{}", br_name, port_name, vlan_id);
             let add_result = c.add_port(br_name, port_name, &OvsPortMode::Access(vlan_id));
             match add_result {
                 Ok(add_result) =>{
