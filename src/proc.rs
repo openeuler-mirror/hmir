ddc63837ac0c74728f2c590942427fe88e33d7cb
duanwujie88 <dev17001@linx-info.com>
2022-12-07 18:28:11 +0800

    增加process接口

--- src/proc.rs
+++ src/proc.rs
@@ -1,13 +1,27 @@
 //! 进程管理模块
 //!
 //!
-
+//! 支持以下的请求
+//! - process-all : 查询指定服务状态
+//!
+//! 请求格式:
+//! ```
+//! {
+//!    "jsonrpc":"2.0",
+//!    "id":1,
+//!    "method":"process-all"
+//! }
+//! ```
 
 use jsonrpsee::ws_server::{RpcModule};
 
 use procfs;
 use serde::{Deserialize, Serialize};
 use hmir_hash::HashWrap;
+use std::string;
+use std::error::Error;
+use nix::sys::signal;
+use nix::unistd;
 
 
 #[derive(Clone, Debug,Serialize)]
@@ -35,16 +49,59 @@ pub fn process_all() -> std::string::String
     serialized
 }
 
-pub fn process_status() -> std::string::String {
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn process_all_it_works() {
+        let s = process_all();
+        println!("{}",s);
+    }
 
-    todo!()
-
+    #[test]
+    fn process_status_it_works(){
+        let s = process_status(0);
+        println!("{}",s);
+    }
 }
 
-pub fn process_kill() -> std::string::String {
-    todo!()
+pub fn process_status(pid : i32) -> std::string::String {
+    if is_valid_process(pid) {
 
+        let process = procfs::process::Process::new(pid);
+        let stat = process.unwrap().stat().unwrap();
+        let p  = ProcInfo {
+            pid: stat.pid,
+            comm: stat.comm
+        };
+        let mut map = HashWrap::<i32,ProcInfo>:: new();
+        map.insert(stat.pid,p);
+        let serialized = serde_json::to_string(&map).unwrap();
+        return serialized;
+    }
+    return string::String::from("Invalid process");
+}
+
+
+fn is_valid_process(pid : i32) -> bool {
+    let process = procfs::process::Process::new(pid);
+    match process {
+        Err(e) => {
+            return false;
+        },
+        _ => {
+            return true;
+        }
+    }
+}
+
+pub fn process_kill(pid : i32) -> std::string::String {
+    if is_valid_process(pid) {
+        signal::kill(unistd::Pid::from_raw(pid), signal::Signal::SIGTERM).unwrap();
+        return string::String::from("Ok");
+    }
+    return string::String::from("Invalid process");
 }
 
 
@@ -55,6 +112,13 @@ pub fn register_method(module :  & mut RpcModule<()>) -> anyhow::Result<()> {
         //默认没有error就是成功的
         Ok(process_all())
     })?;
+
+    module.register_method("process-status", |params, _| {
+        //默认没有error就是成功的
+        let pid = params.one::<i32>()?;
+        Ok(process_status(pid))
+    })?;
+
 
     Ok(())
 }
