7793bedd9f9eea08cbbc442f794bd64eae41eb49
ckxu <dev17302@linx-info.com>
2022-12-14 14:35:11 +0800

    ovs-vsctl:增加add-port与del-port能力

--- hmir-ovs/src/ovs_ofctl.rs
+++ hmir-ovs/src/ovs_ofctl.rs
@@ -2,6 +2,8 @@ use std::process::Command;
 use std::collections::HashMap;
 use jsonrpsee::ws_server::RpcModule;
 
+const OFCTL_CMD: &str= "ovs-ofctl";
+
 
 pub fn register_method(module :  & mut RpcModule<()>) -> anyhow::Result<()>{
 
