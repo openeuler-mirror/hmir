f1f0f3a252a061cf5fc7fc47ca28d36a84a33acf
ckxu <dev17302@linx-info.com>
2022-12-14 10:45:07 +0800

    ovs增加ofctl模块

--- hmir-ovs/src/lib.rs
+++ hmir-ovs/src/lib.rs
@@ -4,3 +4,4 @@ pub mod ovs_bridge;
 pub mod ovs_port;
 pub mod ovs_client;
 pub mod ovs_error;
+pub mod ovs_ofctl;
---
+++ hmir-ovs/src/ovs_ofctl.rs
@@ -0,0 +1,9 @@
+use std::process::Command;
+use std::collections::HashMap;
+use jsonrpsee::ws_server::RpcModule;
+
+
+pub fn register_method(module :  & mut RpcModule<()>) -> anyhow::Result<()>{
+
+    Ok(())
+}
