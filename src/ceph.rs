3d0cd69780f2f1998dcb24af0736abbd614234af
zhang_tianran <dev17101@linx-info.com>
2022-12-20 15:53:19 +0800

    任务说明: 新功能,导入ceph模块,添加ceph集群状态获取
    作者: yuzhang
    
    日期: 2022/12/20

--- src/ceph.rs
+++ src/ceph.rs
@@ -2,11 +2,21 @@
 //!
 //!
 
-use jsonrpsee::ws_server::{RpcModule};
-
+use jsonrpsee::ws_server::{RpcModule, WsServerBuilder,WsServerHandle};
+use hmir_ceph::ceph_client;
 
 #[doc(hidden)]
 pub fn register_method(module :  & mut RpcModule<()>) -> anyhow::Result<()> {
     //The svr module
+
+    module.register_method("ceph-cluster-stat", |_, _| {
+        //获取ceph集群状态
+        Ok(ceph_cluster_stat())
+    })?;
+
     Ok(())
+}
+
+pub fn ceph_cluster_stat() -> String {
+    ceph_client::ceph_status()
 }
