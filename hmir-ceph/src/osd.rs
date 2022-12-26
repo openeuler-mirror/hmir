947f69d3292779d4d0d8fa5f56d56ca4bb5710e9
zhang_tianran <dev17101@linx-info.com>
2022-12-26 18:06:11 +0800

    任务说明: 新增osd延迟测试接口,修改crush rule的错误接口调用
    作者: yuzhang
    
    日期: 2022/12/26

--- hmir-ceph/src/osd.rs
+++ hmir-ceph/src/osd.rs
@@ -14,12 +14,17 @@ pub fn osd_versions() -> String {
     command::mon_exec("osd versions")
 }
 
-//osd组件元数据信息查询
+///osd组件元数据信息查询
 pub fn osd_metadata() -> String {
     command::mon_exec("osd metadata")
 }
 
-//crush规则查询
+///osd延迟测试接口
+pub fn osd_perf() -> String {
+    command::mon_exec("osd perf")
+}
+
+///crush规则查询
 pub fn osd_crush_rule_dump() -> String {
     command::mon_exec("osd crush rule dump")
 }
