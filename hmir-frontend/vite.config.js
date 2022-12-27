03d670513fbd4aa7d94cce4603548b6a1766c53a
zhang_tianran <dev17101@linx-info.com>
2022-12-27 14:16:54 +0800

    任务说明：增加登录页输入清空功能；统一前端代码格式；相对路径别名配置，使用 @ 代替 src
    任务类型：功能开发
    
    日期：2022-12-27

--- hmir-frontend/vite.config.js
+++ hmir-frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -23,4 +24,9 @@ export default defineConfig({
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
   },
+  resolve: {
+    alias: {
+        "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
+    }
+  }
 });
