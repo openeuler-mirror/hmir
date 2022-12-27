1ad1ccd9b0d99e080ef00ca477d17c11abc48403
duanwujie88 <dev17001@linx-info.com>
2022-12-27 18:05:42 +0800

    添加前端登录验证接口

--- hmir-frontend/src-tauri/src/main.rs
+++ hmir-frontend/src-tauri/src/main.rs
@@ -11,26 +11,34 @@ use futures::executor::block_on;
 use tokio;
 
 mod wsclient;
+mod clientmgr;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn ttyd_start(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn cmd_ttyd_start() -> bool {
+    let client = wsclient::RequestClient::new("172.30.24.123:5898".to_string());
+    return client.ttyd_start();
 }
 
 
 #[tauri::command]
 fn greet(name : & str) -> String {
-    let client = wsclient::RequestClient::new("172.30.24.123:5898".to_string());
-    client.ttyd_start();
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn cmd_login(host : & str, port : i32 , username : & str, password : & str) -> bool
+{
+    if clientmgr::register_client(host,port) {
+        return clientmgr::login(host,username,password);
+    }
+    return false;
+}
 
 fn main() {
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet,ttyd_start])
+        .invoke_handler(tauri::generate_handler![greet,cmd_login,cmd_ttyd_start])
         // .invoke_handler(tauri::generate_handler![ttyd_start])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
