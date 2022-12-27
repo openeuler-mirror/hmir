40b78f133fa9a563bb4c40f37ed9b074f7e373ad
duanwujie88 <dev17001@linx-info.com>
2022-12-27 14:55:29 +0800

    增加pam认证登录模块，并添加了测试用例

--- src/pam.rs
+++ src/pam.rs
@@ -1,12 +1,36 @@
 
 
 use pam::Authenticator;
+use jsonrpsee::ws_server::{RpcModule};
+use hmir_hash::HashWrap;
+use serde::{Deserialize};
 
 
-pub fn pam_auth(username: &str, password: &str) -> bool
+#[derive(Debug, Clone,Deserialize)]
+struct LoginParam {
+    username : String,
+    password : String
+}
+
+macro_rules! pam_default_result {
+    ($i:expr) =>{
+        let mut response = HashWrap::<i32,i32>:: new();
+        response.set_code($i);
+        let serialized = serde_json::to_string(&response).unwrap();
+        return serialized;
+    }
+}
+
+pub fn pam_auth(username : &str, password : &str) -> String
 {
+    if is_pam_auth(username,password) {
+        pam_default_result!(0);
+    }
+    pam_default_result!(-1);
+}
 
-    let service = "hmir";
+pub fn is_pam_auth(username: &str, password: &str) -> bool
+{
     let mut authenticator = Authenticator::with_password("system-auth")
         .expect("Failed to init PAM client.");
     authenticator.get_handler().set_credentials(username, password);
@@ -17,6 +41,20 @@ pub fn pam_auth(username: &str, password: &str) -> bool
         return false;
     }
 }
+
+
+#[doc(hidden)]
+pub fn register_method(module :  & mut RpcModule<()>) -> anyhow::Result<()> {
+
+    module.register_method("pam-auth", |params, _| {
+        let login_param = params.parse::<LoginParam>()?;
+        //默认没有error就是成功的
+        Ok(pam_auth(login_param.username.as_str(),login_param.password.as_str()))
+    })?;
+
+    Ok(())
+}
+
 
 
 
@@ -30,6 +68,5 @@ mod tests {
         let auth = pam_auth(&"linx",&"root");
         println!("The auth is : {}",auth);
     }
-
 
 }
