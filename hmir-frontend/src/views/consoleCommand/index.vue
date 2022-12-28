2c2590c4b59f0c90b362cbb92f8b3d5a475d7de6
zhang_tianran <dev17101@linx-info.com>
2022-12-28 11:28:16 +0800

    任务说明:前端增加控制台连接功能
    任务类型：功能开发
    
    日期：2022-12-28

--- hmir-frontend/src/views/consoleCommand/index.vue
+++ hmir-frontend/src/views/consoleCommand/index.vue
@@ -1,13 +1,32 @@
+<template>
+  <div class="card">
+    <button type="button" @click="ttydStop()"  v-if="ttydStartMsg">断开连接</button>
+    <button type="button" @click="ttydStart()" v-else>连接控制台</button>
+  </div>
+  <p>{{ ttydStartMsg }}{{ ttydStopMsg }}</p>
+  <div class="iframe" v-if="ttydStartMsg">
+    <iframe name="iframeMap" id="iframeMapViewComponent" :src="getPageUrl" width="100%" height="100%" frameborder="0"
+      scrolling="yes" ref="iframeDom"></iframe>
+  </div>
+</template>
+
 <script setup>
 import { ref, onMounted } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
 
-const greetMsg = ref("");
+const ttydStartMsg = ref("");
+const ttydStopMsg = ref("");
 const name = ref("");
-const getPageUrl = ref("http://172.30.24.123:5898/");
-async function greet () {
+const getPageUrl = ref("http://172.30.24.123:5899/");
+async function ttydStart () {
+  //连接控制台
+  ttydStartMsg.value = await invoke("cmd_ttyd_start", {});
+  console.log(ttydStartMsg.value);
+}
+async function ttydStop () {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsg.value = await invoke("greet", { name: name.value });
+  ttydStartMsg.value = await invoke("cmd_ttyd_stop", {});
+  console.log(ttydStartMsg.value);
 }
 
 onMounted(() => {
@@ -15,22 +34,12 @@ onMounted(() => {
 });
 
 function handleQuery () {
-  greet()
+  ttydStart()
 }
 </script>
 
 
-<template>
-  <div class="card">
-    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-    <button type="button" @click="greet()">Greet</button>
-  </div>
-  <p>{{ greetMsg }}</p>
-  <div class="iframe">
-    <iframe name="iframeMap" id="iframeMapViewComponent" :src="getPageUrl" width="100%" height="100%" frameborder="0"
-      scrolling="yes" ref="iframeDom"></iframe>
-  </div>
-</template>
+
 <style lang="scss" scoped>
 .iframe {
   height: calc(100vh - 275px);
