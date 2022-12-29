8cd15d1a56819734ec27cf8f3544bec50bac5cd8
ckxu <dev17302@linx-info.com>
2022-12-29 11:12:01 +0800

    ovs-vsctl 命令行执行使用sh -c方式

--- hmir-ovs/src/ovs_common.rs
+++ hmir-ovs/src/ovs_common.rs
@@ -1,5 +1,5 @@
 use std::process::{Output};
-
+use std::process::Command;
 pub fn reflect_cmd_result(output : Output) -> String{
 
     if output.status.success(){
@@ -7,4 +7,13 @@ pub fn reflect_cmd_result(output : Output) -> String{
     } else {
         String::from_utf8_lossy(&output.stderr).to_string()
     }
+}
+
+pub fn exec_rule(rule: String, cmd_name: String) -> String{
+    let output = Command::new("sh")
+                        .arg("-c")
+                        .arg(rule).
+                        output().expect(&format!("failed to excute {}", cmd_name));
+    
+    reflect_cmd_result(output) 
 }
