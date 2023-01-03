0367e75fe1ca49961e6882d34bb3dd429f95be01
zhang_tianran <dev17101@linx-info.com>
2023-01-03 15:52:45 +0800

    增加导航栏用户退出功能页面

---
+++ hmir-frontend/src/layout/header/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="headerClass">
+    <div class="userClass">
+      <el-dropdown trigger="click" @command="handleCommand">
+        <el-button class="el-dropdown-link">
+          <el-icon>
+            <User />
+          </el-icon>
+          root
+        </el-button>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item command="logout">注销</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ElMessage from '@/utils/message.js';
+
+const handleCommand = (command: string | number | object) => {
+  console.log(command);
+}
+</script>
+
+<style lang="scss" scoped>
+.headerClass {
+  position: relative;
+  height: 50px;
+
+  .userClass {
+    display: flex;
+    height: 100%;
+    position: absolute;
+    right: 0;
+    align-items: center;
+  }
+}
+</style>
