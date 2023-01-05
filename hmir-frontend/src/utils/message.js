ac53c328ea195a707c7abfe99c65ba18a48a7a4b
zhang_tianran <dev17101@linx-info.com>
2023-01-05 13:25:55 +0800

    解决当使用message传过来的数据为字符串时无法正常解析的问题

--- hmir-frontend/src/utils/message.js
+++ hmir-frontend/src/utils/message.js
@@ -3,13 +3,25 @@ import { ElMessage } from 'element-plus'
 function elMessage (message) {
   //清除之前的所有的消息提示，解决一次性弹出多个消息提示的问题
   ElMessage.closeAll()
-  //将传过来的数据与默认参数进行替换合并
-  let option = Object.assign({
+  let option = {
+    message: '',
     type: 'success',
     center: true,
     customClass: 'login-message-success',
     offset: 50
-  }, message)
+  }
+  //判断传过来的数据类型是string还是object
+  if (typeof message === 'string') {
+    option.message = message
+  } else if (typeof message === 'object') {
+    //将传过来的数据与默认参数进行替换合并
+    Object.assign(option, message)
+  } else {
+    Object.assign(option, {
+      message: '未识别的错误信息',
+      type: 'error'
+    })
+  }
   //执行弹出消息提示函数
   return ElMessage(option)
 }
