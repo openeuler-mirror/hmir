738f8b672d037ce17c1b697c4b16abf7a9d57cac
zhang_tianran <dev17101@linx-info.com>
2023-01-05 13:14:44 +0800

    优化message函数，解决一次性弹出多个消息提示的问题

--- hmir-frontend/src/utils/message.js
+++ hmir-frontend/src/utils/message.js
@@ -1,13 +1,18 @@
 import { ElMessage } from 'element-plus'
 
 function elMessage (message) {
-  let value = Object.assign({
+  //清除之前的所有的消息提示，解决一次性弹出多个消息提示的问题
+  ElMessage.closeAll()
+  //将传过来的数据与默认参数进行替换合并
+  let option = Object.assign({
     type: 'success',
     center: true,
     customClass: 'login-message-success',
     offset: 50
   }, message)
-  return ElMessage(value)
+  //执行弹出消息提示函数
+  return ElMessage(option)
 }
 
+//导出函数
 export default elMessage
