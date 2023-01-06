3738cc7c284f4ca68c9452633e4cc78f66feed60
zhang_tianran <dev17101@linx-info.com>
2023-01-06 11:25:01 +0800

    增加状态管理仓库登录函数

--- hmir-frontend/src/store/modules/user.ts
+++ hmir-frontend/src/store/modules/user.ts
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia';
 
+// 第一个参数是应用程序中 store 的唯一 id
 export const useUsersStore = defineStore('user', {
   //存储用户数据
   state: () => {
@@ -12,7 +13,29 @@ export const useUsersStore = defineStore('user', {
   //计算属性
   getters: {
   },
-  //异步函数
+  //异步同步函数
   actions: {
-  },
+    cmdLogin(loginData) {
+      return new Promise<void>((resolve, reject) => {
+        invoke("cmd_login", { ...loginData })
+          .then(response => {
+            console.log(response);
+            if (response) {
+              ElMessage.success('登录成功')
+              resolve();
+            } else {
+              ElMessage({
+                message: '登录失败，请重试',
+                type: 'error',
+                customClass: 'login-message-error',
+              })
+              reject(response);
+            }
+          })
+          .catch(error => {
+            reject(error);
+          });
+      })
+    }
+  }
 });
