31451247fe5b348d949658eddcf976281d92c325
duanwujie88 <dev17001@linx-info.com>
2023-01-06 23:26:25 +0800

    添加sysctl lib crate

---
+++ hmir-sysctl/src/lib.rs
@@ -0,0 +1,29 @@
+use sysctl::Sysctl;
+
+pub fn sysctl_os_type() -> String
+{
+    #[cfg(any(target_os = "macos", target_os = "ios", target_os = "freebsd"))]
+    const CTLNAME: &str = "kern.ostype";
+
+    #[cfg(any(target_os = "linux", target_os = "android"))]
+    const CTLNAME: &str = "kern.ostype";
+
+    let ctl = sysctl::Ctl::new(CTLNAME).unwrap();
+    let desc = ctl.description().unwrap();
+    return desc;
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
