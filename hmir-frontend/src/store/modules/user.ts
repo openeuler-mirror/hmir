11ec30a0c078deca3de85b0bd6385ea862f0a133
zhang_tianran <dev17101@linx-info.com>
2023-01-06 14:04:14 +0800

    pinia仓库封装用户注销功能函数

--- hmir-frontend/src/store/modules/user.ts
+++ hmir-frontend/src/store/modules/user.ts
@@ -8,6 +8,10 @@ interface LoginData {
   password: string;
 }
 
+interface logoutData {
+  host: string;
+}
+
 // 第一个参数是应用程序中 store 的唯一 id
 export const useUsersStore = defineStore('user', {
   //存储用户数据
@@ -27,16 +31,15 @@ export const useUsersStore = defineStore('user', {
       return new Promise<void>((resolve, reject) => {
         invoke("cmd_login", { ...loginData })
           .then(response => {
-            console.log(response);
             if (response) {
-              ElMessage.success('登录成功')
+              ElMessage.success('登录成功');
               resolve();
             } else {
               ElMessage({
                 message: '登录失败，请重试',
                 type: 'error',
                 customClass: 'login-message-error',
-              })
+              });
               reject(response);
             }
           })
@@ -44,6 +47,27 @@ export const useUsersStore = defineStore('user', {
             reject(error);
           });
       })
-    }
+    },
+
+    cmdlogout(logoutData: logoutData) {
+      return new Promise<void>((resolve, reject) => {
+        invoke("cmd_logout", { ...logoutData })
+          .then(response => {
+            //判断是否注销成功
+            if (!response) {
+              ElMessage.success('注销成功');
+            } else {
+              ElMessage.error({
+                message: '注销失败,请联系管理员',
+                customClass: 'login-message-error'
+              });
+            }
+            resolve();
+          })
+          .catch(error => {
+            reject(error);
+          });
+      })
+    },
   }
 });
