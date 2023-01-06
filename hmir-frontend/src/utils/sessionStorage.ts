69848c232a3afcccd91fef118eae66a5a007bbe3
zhang_tianran <dev17101@linx-info.com>
2023-01-06 15:48:54 +0800

    限制本地存储传入的数据类型

--- hmir-frontend/src/utils/sessionStorage.ts
+++ hmir-frontend/src/utils/sessionStorage.ts
@@ -3,7 +3,7 @@
  */
 export const sessionStorage = {
   // 设置临时缓存
-  set (key, val) {
+  set(key: string, val: any) {
     try {
       const newStateValue = typeof val === "object" ? JSON.stringify(val) : val;
       window.sessionStorage.setItem(key, newStateValue);
@@ -13,7 +13,7 @@ export const sessionStorage = {
     }
   },
   // 获取临时缓存
-  get (key) {
+  get(key: string) {
     try {
       const json = window.sessionStorage.getItem(key);
       return json ? JSON.parse(json) : key;
@@ -22,11 +22,11 @@ export const sessionStorage = {
     }
   },
   // 移除临时缓存
-  remove (key) {
+  remove(key: string) {
     window.sessionStorage.removeItem(key);
   },
   // 移除全部临时缓存
-  clear () {
+  clear() {
     window.sessionStorage.clear();
   }
 };
