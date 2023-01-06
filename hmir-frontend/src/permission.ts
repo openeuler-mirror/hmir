ee4dfca26ab74b756ae59853f9aadc6df756e450
zhang_tianran <dev17101@linx-info.com>
2023-01-06 17:31:29 +0800

    添加路由判断，只有用户登录后才能进入主页

--- hmir-frontend/src/permission.ts
+++ hmir-frontend/src/permission.ts
@@ -1,11 +1,27 @@
 import router from '@/router';
-import { RouteRecordRaw } from 'vue-router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
+import { sessionStorage } from '@/utils/sessionStorage'
 NProgress.configure({ showSpinner: false }); // 进度条
+
+// 白名单路由
+const whiteList = ['/login'];
 
 router.beforeEach((to, from, next) => {
   NProgress.start();
+  let value = sessionStorage.get('user')
+  //判断用户是否登录
+  if (value !== 'user') {
+    next();
+  } else {
+    // 未登录可以访问白名单页面
+    if (whiteList.includes(to.path)) {
+      next();
+    } else {
+      next(`/login`);
+      NProgress.done();
+    }
+  }
 });
 
 router.afterEach(() => {
