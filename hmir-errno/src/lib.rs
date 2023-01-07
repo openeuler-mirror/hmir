0a2fdf4091c34e70c932554f7c216b9b2fa41444
duanwujie88 <dev17001@linx-info.com>
2023-01-07 10:03:17 +0800

    添加错误码 lib crate

---
+++ hmir-errno/src/lib.rs
@@ -0,0 +1,21 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+
+pub mod errno {
+    pub static HMIR_SUCCESS: i32 = 0;
+
+
+}
