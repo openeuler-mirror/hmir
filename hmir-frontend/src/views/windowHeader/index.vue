060a990179696edb9ecf422bf5b76bc7e99410ca
zhang_tianran <dev17101@linx-info.com>
2023-01-10 11:01:45 +0800

    点击帮助-关于按钮后跳转到关于页面

--- hmir-frontend/src/views/windowHeader/index.vue
+++ hmir-frontend/src/views/windowHeader/index.vue
@@ -9,7 +9,7 @@
       </el-sub-menu>
       <el-sub-menu index="1" :popper-offset="0">
         <template #title>帮助</template>
-        <el-menu-item index="1-1" class="el-menu-item-height">关于</el-menu-item>
+        <el-menu-item index="about" class="el-menu-item-height">关于</el-menu-item>
       </el-sub-menu>
       <div class="flex-grow" />
     </el-menu>
@@ -18,21 +18,31 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { invoke } from "@tauri-apps/api/tauri";
+import { useRouter } from 'vue-router';
+//引入路由
+const router = useRouter()
 
+//下拉框通过什么触发
 const menuTrigger = ref<string>('click')
+
 // 菜单激活回调	
 const handleSelect = (key: string, keyPath: string[]) => {
   if (key === 'processQuit') {
     processQuit()
   }
+  if (key === 'about') {
+    openAboutWindow()
+  }
   console.log(key, keyPath)
 }
+
 //  展开的回调
 const handleOpen = (key: string, keyPath: string[]) => {
   // 只要有一个菜单展开后将菜单展开的触发条件改为hover触发
   menuTrigger.value = 'hover'
   console.log(key, keyPath)
 }
+
 // 收起的回调
 const handleClose = (key: string, keyPath: string[]) => {
   // 所有菜单关闭后将菜单展开的触发条件改为click触发
@@ -41,14 +51,16 @@ const handleClose = (key: string, keyPath: string[]) => {
   // processQuit()
 }
 
-
-
+//退出
 async function processQuit() {
   //点击退出后关闭窗口
   await invoke("cmd_quit", {});
 }
 
-
+//关于窗口
+function openAboutWindow(){
+  router.push({ path: '/about' })
+}
 
 </script>
 
