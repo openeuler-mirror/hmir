8eb75a8f521befec877dc5b9acd73498ebfcb1a9
zhang_tianran <dev17101@linx-info.com>
2023-01-10 17:59:27 +0800

    将连接控制台时的IP地址修改为登录时的IP地址

--- hmir-frontend/src/views/consoleCommand/index.vue
+++ hmir-frontend/src/views/consoleCommand/index.vue
@@ -3,7 +3,6 @@
     <button type="button" @click="ttydStop()" v-if="ttydtMsg">断开连接</button>
     <button type="button" @click="ttydStart()" v-else>连接控制台</button>
   </div>
-  <p>{{ ttydtMsg }}</p>
   <div class="iframe">
     <iframe name="iframeMap" id="iframeMapViewComponent" :src="getPageUrl" width="100%" height="100%" frameborder="0"
       scrolling="yes" ref="iframeDom" v-if="ttydtMsg"></iframe>
@@ -12,19 +11,37 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
+import ElMessage from '@/utils/message';
 import { invoke } from "@tauri-apps/api/tauri";
+import { useUsersStore } from '@/store/modules/user';
 
+//判断控制台是否显示
 const ttydtMsg = ref("");
-const name = ref("");
-const getPageUrl = ref("http://172.30.24.123:5899/");
+
+//引入store仓库
+const store = useUsersStore();
+
+//连接终端的地址
+const getPageUrl = ref("");
+
+//连接终端
 async function ttydStart () {
   //连接控制台
-  ttydtMsg.value = await invoke("cmd_ttyd_start", { host: '172.30.24.123' });
+  ttydtMsg.value = await invoke("cmd_ttyd_start", { host: store.host });
+  if (!ttydtMsg.value) {
+    ElMessage({
+      message: '连接失败，请重试',
+      type: 'error',
+      customClass: 'login-message-error',
+    });
+  }
   console.log(ttydtMsg.value);
 }
+
+//断开终端
 async function ttydStop () {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  let value = await invoke("cmd_ttyd_stop", { host: '172.30.24.123' });
+  let value = await invoke("cmd_ttyd_stop", { host: store.host });
   if (value) {
     ttydtMsg.value = false
   }
@@ -32,12 +49,10 @@ async function ttydStop () {
 }
 
 onMounted(() => {
-  // handleQuery();
+  getPageUrl.value = `http://${store.host}:5899`
+  ttydStart();
 });
 
-function handleQuery () {
-  ttydStart()
-}
 </script>
 
 
