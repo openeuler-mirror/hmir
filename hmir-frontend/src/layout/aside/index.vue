4a4dc49a5180f27d37b084ad0c52d68444344209
zhang_tianran <dev17101@linx-info.com>
2023-01-11 16:57:55 +0800

    根据动态添加的路由来渲染左侧菜单栏

--- hmir-frontend/src/layout/aside/index.vue
+++ hmir-frontend/src/layout/aside/index.vue
@@ -2,12 +2,11 @@
   <div id="menu">
     <el-menu ref="menuHeader" :default-active="handleValue" class="el-menu-vertical-demo-header" :collapse="isCollapse"
       @open="handleOpen" @close="handleClose" @select="handleSelect" router>
-      <el-menu-item :index="item.indexRouter" v-for="item in handleRouter" :key="item.indexRouter"
-        :disabled="item.disabled">
+      <el-menu-item :index="item.path" v-for="item in handleRouter" :key="item.name" :disabled="item.meta.disabled">
         <el-icon>
-          <component :is="item.icon" style="width: 16px;height: 16px;"></component>
+          <component :is="item.meta.icon" style="width: 16px;height: 16px;"></component>
         </el-icon>
-        <template #title>{{ item.title }}</template>
+        <template #title>{{ item.meta.title }}</template>
       </el-menu-item>
       <el-affix position="bottom" :offset="0" target="#menu" class="classFooter">
         <el-menu-item index="console" class="classFooter">
@@ -17,15 +16,6 @@
           <template #title>控制台</template>
         </el-menu-item>
       </el-affix>
-
-      <!-- <el-menu-item index="home">
-        <el-icon><icon-menu /></el-icon>
-        <template #title>仪表板</template>
-      </el-menu-item>
-      <el-menu-item index="console">
-        <el-icon><setting /></el-icon>
-        <template #title>Navigator Four</template>
-      </el-menu-item> -->
     </el-menu>
   </div>
 </template>
@@ -33,20 +23,17 @@
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import { useUsersStore } from '@/store/modules/user';
-import { storeToRefs } from 'pinia';
+import { useRouterStore } from '@/store/modules/router';
+import { computed } from 'vue';
 //引入store仓库
-const store = useUsersStore();
+const routerStore = useRouterStore();
 
-const { host } = storeToRefs(store)
+//左侧菜单栏信息
+const handleRouter = ref<any>()
 
-const handleRouter = ref([{ indexRouter: 'login', title: host.value || '127.0.0.1', disabled: true, icon: 'Menu' },
-{ indexRouter: 'home', title: '服务', disabled: false, icon: 'Menu' },
-])
 //左侧菜单栏是否关闭打开
-const isCollapse = ref(false)
-//进入后的初始页
-const handleValue = ref('home')
+const isCollapse = ref<Boolean>(false)
+
 //引入路由
 const router = useRouter()
 
@@ -64,8 +51,7 @@ const handleSelect = (key: string, keyPath: string[]) => {
 }
 
 function handleQuery() {
-  console.log(handleValue);
-  router.push({ path: '/' + handleValue.value })
+  handleRouter.value = routerStore.userouter
 }
 
 onMounted(() => {
@@ -86,6 +72,7 @@ onMounted(() => {
 
 .classFooter {
   background-color: #fff;
+  color: #000;
   position: absolute;
   bottom: 0px;
 }
