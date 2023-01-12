5a744c6f267e25b658f1b79a5b7bf2bb65c56b87
zhang_tianran <dev17101@linx-info.com>
2023-01-12 17:21:56 +0800

    任务说明: 新功能,新增获取存储池特性参数的接口
    作者: yuzhang
    
    日期: 2023/01/12

--- hmir-ceph/src/pool.rs
+++ hmir-ceph/src/pool.rs
@@ -1,4 +1,9 @@
+use std::borrow::Borrow;
 use crate::command;
+use crate::ceph_client;
+use ceph::{cmd, PoolOption, error::RadosResult};
+// use serde_json::Value::String;
+use std::string::String;
 
 ///存储池列表
 pub fn pool_list() -> String {
@@ -8,4 +13,85 @@ pub fn pool_list() -> String {
 ///存储池状态
 pub fn pool_stats() -> String {
     command::mon_exec("osd pool stats")
+}
+
+//根据pool option字符串返回具体对应的枚举类型
+fn get_pool_option(option: &str) -> Option<PoolOption> {
+    match option {
+        "size" => Some(PoolOption::Size),
+        "min_size" => Some(PoolOption::MinSize),
+        "crash_replay_interval" => Some(PoolOption::CrashReplayInterval),
+        "pg_num" => Some(PoolOption::PgNum),
+        "pgp_num" => Some(PoolOption::PgpNum),
+        "crush_rule" => Some(PoolOption::CrushRule),
+        "hashpspool" => Some(PoolOption::HashPsPool),
+        "nodelete" => Some(PoolOption::NoDelete),
+        "nopgchange" => Some(PoolOption::NoPgChange),
+        "nosizechange" => Some(PoolOption::NoSizeChange),
+        "write_fadvice_dontneed" => Some(PoolOption::WriteFadviceDontNeed),
+        "noscrub" => Some(PoolOption::NoScrub),
+        "nodeep-scrub" => Some(PoolOption::NoDeepScrub),
+        "hit_set_type" => Some(PoolOption::HitSetType),
+        "hit_set_period" => Some(PoolOption::HitSetPeriod),
+        "hit_set_count" => Some(PoolOption::HitSetCount),
+        "hit_set_fpp" => Some(PoolOption::HitSetFpp),
+        "use_gmt_hitset" => Some(PoolOption::UseGmtHitset),
+        "target_max_bytes" => Some(PoolOption::TargetMaxBytes),
+        "target_max_objects" => Some(PoolOption::TargetMaxObjects),
+        "cache_target_dirty_ratio" => Some(PoolOption::CacheTargetDirtyRatio),
+        "cache_target_dirty_high_ratio" => Some(PoolOption::CacheTargetDirtyHighRatio),
+        "cache_target_full_ratio" => Some(PoolOption::CacheTargetFullRatio),
+        "cache_min_flush_age" => Some(PoolOption::CacheMinFlushAge),
+        "cachem_min_evict_age" => Some(PoolOption::CacheMinEvictAge),
+        "auid" => Some(PoolOption::Auid),
+        "min_read_recency_for_promote" => Some(PoolOption::MinReadRecencyForPromote),
+        "min_write_recency_for_promote" => Some(PoolOption::MinWriteRecencyForPromte),
+        "fast_read" => Some(PoolOption::FastRead),
+        "hit_set_decay_rate" => Some(PoolOption::HitSetGradeDecayRate),
+        "hit_set_search_last_n" => Some(PoolOption::HitSetSearchLastN),
+        "scrub_min_interval" => Some(PoolOption::ScrubMinInterval),
+        "scrub_max_interval" => Some(PoolOption::ScrubMaxInterval),
+        "deep_scrub_interval" => Some(PoolOption::DeepScrubInterval),
+        "recovery_priority" => Some(PoolOption::RecoveryPriority),
+        "recovery_op_priority" => Some(PoolOption::RecoveryOpPriority),
+        "scrub_priority" => Some(PoolOption::ScrubPriority),
+        "compression_mode" => Some(PoolOption::CompressionMode),
+        "compression_algorithm" => Some(PoolOption::CompressionAlgorithm),
+        "compression_required_ratio" => Some(PoolOption::CompressionRequiredRatio),
+        "compression_max_blob_size" => Some(PoolOption::CompressionMaxBlobSize),
+        "compression_min_blob_size" => Some(PoolOption::CompressionMinBlobSize),
+        "csum_type" => Some(PoolOption::CsumType),
+        "csum_min_block" => Some(PoolOption::CsumMinBlock),
+        "csum_max_block" => Some(PoolOption::CsumMaxBlock),
+        "allow_ec_overwrites" => Some(PoolOption::AllocEcOverwrites),
+        &_ => None
+    }
+}
+
+// Query a ceph pool.
+pub fn get_param(pool_name: &str, option: &str) -> RadosResult<String> {
+    let client = ceph_client::get_ceph_client()?;
+    let pool_option = get_pool_option(option);
+    match pool_option { 
+        Some(option) => cmd::osd_pool_get(&client, 
+                                          pool_name, 
+                                          &option),
+        None => RadosResult::Ok(String::from("Option not support"))
+    }
+}
+
+#[test]
+pub fn test_pool_option() {
+    println!("{}", get_pool_option("size").unwrap());
+    println!("{}", get_pool_option("size").unwrap());
+}
+
+#[test]
+pub fn test_pool_option1() {
+    let mut vec_s = vec![(String::from("a"), String::from("disable")), 
+                         (String::from("b"), String::from("enable"))];
+    let vec = vec_s.into_iter().filter(|x| x.1.eq("disable"))
+        .map(|x| x.0).collect::<Vec<String>>();
+    let vec2 = vec.iter().map(String::as_str).collect::<Vec<&str>>();
+    println!("{:?}", vec2);
 }
