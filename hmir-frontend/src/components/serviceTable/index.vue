4ee9a273a5c0189f553875aa73060d43b7be5c4f
zhang_tianran <dev17101@linx-info.com>
2023-01-12 19:18:41 +0800

    table相关优化，解决数据过多造成的卡顿问题

--- hmir-frontend/src/components/serviceTable/index.vue
+++ hmir-frontend/src/components/serviceTable/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <el-table :data="tableList" style="width: 100%;height: 40vh;" empty-text="暂无数据" highlight-current-row
+  <div style="height: 40vh;width: 100%;">
+    <!-- <el-table :data="tableList" style="width: 100%;height: 40vh;" empty-text="暂无数据" highlight-current-row
       @current-change="handleCurrentChange">
       <el-table-column v-for="item of tableProp" :prop="item.prop" :label="item.label" />
       <el-table-column prop="state" label="状态">
@@ -9,14 +9,18 @@
           </div>
         </template>
       </el-table-column>
-    </el-table>
+    </el-table> -->
+    <el-auto-resizer>
+      <template #default="{ height, width }">
+        <el-table-v2 :columns="columns" :data="data" :width="width" :height="height" fixed />
+      </template>
+    </el-auto-resizer>
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref, onMounted, computed } from 'vue';
-import { ElTable } from 'element-plus';
-
+<script setup lang="tsx">
+import { ref, onMounted, computed, nextTick } from 'vue';
+import type { Column } from 'element-plus'
 const props = defineProps({
   tableList: {
     type: Array,
@@ -28,27 +32,55 @@ const props = defineProps({
   }
 });
 
-//过滤转化为中文
-const stateFilter = computed(() => {
-  // value是计算属性执行后，再次执行return里面的函数时传的参数
-  return (value: any) => {
-    let state = value
-    switch (value) {
-      case 'Active':
-        state = "激活";
-        break;
-    }
-    return state
+const columns: Column<any>[] = [
+  {
+    key: 'description',
+    title: '描述',
+    dataKey: 'description',
+    width: 300,
+  },
+  {
+    key: 'name',
+    title: 'ID',
+    dataKey: 'name',
+    width: 300,
+  },
+  {
+    key: 'active_state',
+    title: '状态',
+    width: 300,
+    cellRenderer: ({ rowData }) => {
+      //过滤转化为中文
+      const stateFilter = (value: any) => {
+        let state = value
+        switch (value) {
+          case 'Active':
+            state = "激活";
+            break;
+        }
+        return state
+      }
+      return <div value={rowData.checked} >
+        {stateFilter(rowData.active_state)}{rowData.sub_state ? `(${rowData.sub_state})` : ''}
+      </div>
+    },
   }
-})
+]
+
+const data = ref<any>([])
 
 
-const handleCurrentChange = (val: any | undefined) => {
-  console.log(val);
-}
+
+
+// const handleCurrentChange = (val: any | undefined) => {
+//   console.log(val);
+// }
 
 onMounted(() => {
-  // console.log(props.tableList, props.tableProp);
+  setTimeout(() => {
+    console.log(props.tableList);
+    data.value = props.tableList
+  }, 700);
 })
 </script>
 
