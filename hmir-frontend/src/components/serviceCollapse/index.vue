e9eadd1e408a6c46570022438895fb4b2821dee2
zhang_tianran <dev17101@linx-info.com>
2023-01-12 09:39:41 +0800

    serviceCollapse组件封装

---
+++ hmir-frontend/src/components/serviceCollapse/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="demo-collapse">
+    <el-collapse v-model="activeName" accordion>
+      <el-collapse-item v-for="item of description" :key="item.value" :title="item.value" :name="item.value">
+        <div>
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, nextTick } from 'vue'
+
+const props = defineProps({
+  description: {
+    type: Array<any>,
+    default: [{ value: '' }]
+  },
+});
+
+//默认展开项
+const activeName = ref('')
+
+onMounted(() => {
+  nextTick(() => {
+    activeName.value = props.description[0].value;
+  })
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
