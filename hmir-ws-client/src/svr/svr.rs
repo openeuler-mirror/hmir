1b408fe811499e9fd641a265f664058ce92724dc
duanwujie88 <dev17001@linx-info.com>
2023-01-17 17:46:24 +0800

    wsclient增加接口

--- hmir-ws-client/src/svr/svr.rs
+++ hmir-ws-client/src/svr/svr.rs
@@ -24,8 +24,7 @@ use hmir_systemd::{
 
 impl RequestClient {
 
-    fn _svr_get_unit(&self,cmd: &str) -> (usize,String)
-    {
+    fn _svr_get_unit(&self,cmd: &str) -> (usize,String) {
         let token = self.token.clone();
         let (state,service) = self.runtime.block_on(async{
             let response: Result<String, _> = self.client.request(cmd, rpc_params![token]).await;
@@ -66,6 +65,29 @@ impl RequestClient {
         self._svr_get_unit("svr-list-static-timer")
     }
 
+    pub fn svr_list_enabled_socket(&self) -> (usize,String) {
+        self._svr_get_unit("svr-list-enabled-socket")
+    }
+
+    pub fn svr_list_disabled_socket(&self) -> (usize,String) {
+        self._svr_get_unit("svr-list-disabled-socket")
+    }
+
+    pub fn svr_list_static_socket(&self) -> (usize,String) {
+        self._svr_get_unit("svr-list-static-socket")
+    }
+
+    pub fn svr_list_enabled_target(&self) -> (usize,String) {
+        self._svr_get_unit("svr-list-enabled-target")
+    }
+
+    pub fn svr_list_disabled_target(&self) -> (usize,String) {
+        self._svr_get_unit("svr-list-disabled-target")
+    }
+
+    pub fn svr_list_static_target(&self) -> (usize,String) {
+        self._svr_get_unit("svr-list-static-target")
+    }
 
 }
 
@@ -77,7 +99,13 @@ mod tests {
 
     const URL : &str = "127.0.0.1:5899";
 
-
+    // macro_rules! svr_print_result {
+    //     ($i:expr) =>{
+    //                 let p: HashWrap::<String,HmirUnit> = serde_json::from_str(i.as_str()).unwrap();
+    //
+    //
+    //     }
+    // }
 
     #[test]
     fn svr_list_enable_service_worked(){
@@ -114,6 +142,102 @@ mod tests {
                 c.login("root","root");
                 let (result,state) = c.svr_list_enabled_timer();
                 println!("{}",result);
+            }
+            _ => {}
+        }
+    }
+
+    #[test]
+    fn svr_list_enabled_socket_worked(){
+        let client = RequestClient::new(String::from(URL));
+
+
+        match client {
+            Ok(mut c) => {
+                c.login("root","root");
+                let (result,state) = c.svr_list_enabled_socket();
+                println!("{}",result);
+            }
+            _ => {}
+        }
+    }
+
+
+
+    #[test]
+    fn svr_list_disabled_socket_worked(){
+        let client = RequestClient::new(String::from(URL));
+
+
+        match client {
+            Ok(mut c) => {
+                c.login("root","root");
+                let (result,state) = c.svr_list_disabled_socket();
+                println!("{}",result);
+            }
+            _ => {}
+        }
+    }
+
+
+
+    #[test]
+    fn svr_list_static_socket_worked(){
+        let client = RequestClient::new(String::from(URL));
+
+
+        match client {
+            Ok(mut c) => {
+                c.login("root","root");
+                let (state,result) = c.svr_list_static_socket();
+                println!("{}",result);
+            }
+            _ => {}
+        }
+    }
+
+
+    #[test]
+    fn svr_all_worked(){
+        let client = RequestClient::new(String::from(URL));
+
+
+        match client {
+            Ok(mut c) => {
+                c.login("root","root");
+                // let (state,result) = c.svr_list_enabled_service();
+                // println!("{}",result);
+                // let (state,result) = c.svr_list_disabled_service();
+                // println!("{}",result);
+                // let (state,result) = c.svr_list_static_service();
+                // println!("{}",result);
+
+
+                let (state,result) = c.svr_list_enabled_socket();
+                println!("{}",result);
+                let (state,result) = c.svr_list_disabled_socket();
+                println!("{}",result);
+                let (state,result) = c.svr_list_static_socket();
+                println!("{}",result);
+
+
+                // let (state,result) = c.svr_list_enabled_timer();
+                // println!("{}",result);
+                // let (state,result) = c.svr_list_disabled_timer();
+                // println!("{}",result);
+                // let (state,result) = c.svr_list_static_timer();
+                // println!("{}",result);
+                //
+                //
+                //
+                // let (state,result) = c.svr_list_enabled_target();
+                // println!("{}",result);
+                // let (state,result) = c.svr_list_disabled_target();
+                // println!("{}",result);
+                // let (state,result) = c.svr_list_static_target();
+                // println!("{}",result);
+
+
             }
             _ => {}
         }
