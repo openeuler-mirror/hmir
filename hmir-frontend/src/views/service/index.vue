cf5200811074e58737ab42c3bdb4164625e24cb5
zhang_tianran <dev17101@linx-info.com>
2023-01-28 10:09:57 +0800

    进程页面

--- hmir-frontend/src/views/service/index.vue
+++ hmir-frontend/src/views/service/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-tabs v-model="serviceActive" class="demo-tabs" @tab-click="handleClick">
-      <!-- <el-tab-pane label="目标" name="serviceTarget">
+      <el-tab-pane label="目标" name="serviceTarget">
         <serviceTarget></serviceTarget>
       </el-tab-pane>
       <el-tab-pane label="系统服务" name="systemService">
@@ -15,16 +15,16 @@
       </el-tab-pane>
       <el-tab-pane label="路径" name="servicePath">
         <servicePath></servicePath>
-      </el-tab-pane> -->
-      <el-tab-pane v-for="item of serviceAll" :key="item.name" :label="item.lable" :name="item.name">
-        <div>
+      </el-tab-pane>
+      <!-- <el-tab-pane v-for="item of serviceAll" :key="item.name" :label="item.lable" :name="item.name">
+        <div v-if="(serviceActive === item.name)">
           <router-view v-slot="{ Component, route }">
             <keep-alive>
               <component :is="Component" :key="route.fullPath" />
             </keep-alive>
           </router-view>
         </div>
-      </el-tab-pane>
+      </el-tab-pane> -->
       <el-tab-pane name="" disabled v-if="(serviceActive === 'serviceTimer')">
         <template #label>
           <el-button type="primary" size="small">创建定时器</el-button>
@@ -39,6 +39,13 @@ import { ref, watch, onMounted } from 'vue';
 import type { TabsPaneContext } from 'element-plus';
 import router from '@/router';
 import { cmdServiceStore } from '@/store/modules/service';
+import serviceTarget from '@/views/service/components/serviceTarget/index.vue';
+import systemService from '@/views/service/components/systemService/index.vue';
+import serviceSocket from '@/views/service/components/serviceSocket/index.vue';
+import serviceTimer from '@/views/service/components/serviceTimer/index.vue';
+import servicePath from '@/views/service/components/servicePath/index.vue';
+
+
 
 //引入store仓库
 const store = cmdServiceStore();
@@ -46,18 +53,18 @@ const store = cmdServiceStore();
 //当前点击标签
 const serviceActive = ref('systemService')
 
-const serviceAll = ref([
-  { lable: '目标', name: 'serviceTarget' },
-  { lable: '系统服务', name: 'systemService' },
-  { lable: '套接字', name: 'serviceSocket' },
-  { lable: '计时器', name: 'serviceTimer' },
-  { lable: '路径', name: 'servicePath' },
-])
+// const serviceAll = ref([
+//   { lable: '目标', name: 'serviceTarget' },
+//   { lable: '系统服务', name: 'systemService' },
+//   { lable: '套接字', name: 'serviceSocket' },
+//   { lable: '计时器', name: 'serviceTimer' },
+//   { lable: '路径', name: 'servicePath' },
+// ])
 
 //监听tableList的变化，实时刷新表格
-watch(serviceActive, value => {
-  router.push(`/service/${value}`)
-});
+// watch(serviceActive, value => {
+//   router.push(`/service/${value}`)
+// });
 
 
 const handleClick = (tab: TabsPaneContext, event: Event) => {
