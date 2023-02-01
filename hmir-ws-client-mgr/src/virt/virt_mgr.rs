1640c6fc8bef11d507ffffce03f27550059335dd
ckxu <dev17302@linx-info.com>
2023-02-01 14:07:23 +0800

    hmir-ws-client:添加virt_show_hypervisor接口

--- hmir-ws-client-mgr/src/virt/virt_mgr.rs
+++ hmir-ws-client-mgr/src/virt/virt_mgr.rs
@@ -7,3 +7,10 @@ pub fn virt_check_connection(host: &str) -> (usize,String)
     let h = host.to_string();
     return client_instance!(&h).virt_check_connection();
 }
+
+#[allow(dead_code)]
+pub fn virt_show_hypervisor(host: &str) -> (usize,String)
+{
+    let h = host.to_string();
+    return client_instance!(&h).virt_show_hypervisor();
+}
