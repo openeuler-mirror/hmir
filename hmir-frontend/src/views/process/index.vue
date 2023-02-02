c980d41c4ccd092ee4a58f33c00a33847ce0fe74
duanwujie88 <dev17001@linx-info.com>
2023-02-02 17:38:58 +0800

    添加process API

--- hmir-frontend/src/views/process/index.vue
+++ hmir-frontend/src/views/process/index.vue
@@ -2,22 +2,24 @@
   <div>
     <el-table :data="tableData" style="width: 100%" table-layout="auto" :fit="true" >
       <el-table-column prop="pid" label="进程号" sortable/>
-      <el-table-column prop="user" label="用户名" />
-      <el-table-column prop="priority" label="优先级" />
-      <el-table-column prop="nice" label="nice值" />
-      <el-table-column prop="virt" label="VIRT" />
-      <el-table-column prop="res" label="RES" />
-      <el-table-column prop="shr" label="SHR" />
-      <el-table-column prop="s" label="进程状态" />
-      <el-table-column prop="cpu" label="CPU(%)" />
-      <el-table-column prop="mem" label="MEM(%)" />
-      <el-table-column prop="time" label="TIME+" />
-      <el-table-column prop="command" label="COMMAND" :min-width="100" />
+      <el-table-column prop="user" label="用户名" sortable/>
+      <el-table-column prop="priority" label="优先级" sortable/>
+      <el-table-column prop="nice" label="NICE值" sortable/>
+      <el-table-column prop="virt" label="VIRT" sortable/>
+      <el-table-column prop="res" label="RES" sortable/>
+      <el-table-column prop="shr" label="SHR" sortable/>
+      <el-table-column prop="s" label="进程状态" sortable/>
+      <el-table-column prop="cpu" label="CPU(%)" sortable/>
+      <el-table-column prop="mem" label="MEM(%)" sortable/>
+      <el-table-column prop="time" label="TIME+" sortable/>
+      <el-table-column prop="command" label="COMMAND" :min-width="100" sortable/>
     </el-table>
   </div>
 </template>
 
 <script setup lang="ts">
+
+
 const tableData = [
   {
     pid: 779,
