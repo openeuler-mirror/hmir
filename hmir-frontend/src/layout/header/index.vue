f65010cf5804d7d4d7f36f42ce384b3492e9ea59
zhang_tianran <dev17101@linx-info.com>
2023-02-07 10:41:13 +0800

    修改点击退出时代码错误问题

--- hmir-frontend/src/layout/header/index.vue
+++ hmir-frontend/src/layout/header/index.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: zhang_tianran
+ * @Date: 2022-12-27 16:56:01
+ * @LastEditors: zhang_tianran
+ * @LastEditTime: 2023-02-07 10:11:23
+ * @Description:
+-->
 <template>
   <div class="headerClass">
     <div class="userClass">
@@ -19,34 +26,34 @@
 </template>
 
 <script lang="ts" setup>
-import { useRouter } from 'vue-router';
-import { useUsersStore } from '@/store/modules/user';
-import { storeToRefs } from 'pinia';
-//引入store仓库
-const store = useUsersStore();
+import { useRouter } from 'vue-router'
+import { useUsersStore } from '@/store/modules/user'
+import { storeToRefs } from 'pinia'
+// 引入store仓库
+const store = useUsersStore()
 
-//引入路由
+// 引入路由
 const router = useRouter()
 
 const { host, username } = storeToRefs(store)
 
 const handleCommand = (command: string | number | object) => {
-  //点击退出调用退出功能函数
-  if (command = 'logout') {
+  // 点击退出调用退出功能函数
+  if (command === 'logout') {
     logout()
   }
 }
 
-//退出功能函数
-function logout() {
-  //调用退出功能接口
+// 退出功能函数
+function logout () {
+  // 调用退出功能接口
   store.cmdlogout({ host: host.value })
     .then(res => {
-      //点击注销后跳转到登录页面
+      // 点击注销后跳转到登录页面
       router.push({ path: '/login' })
     })
     .catch(error => {
-      console.log(error);
+      console.log(error)
     })
 }
 </script>
