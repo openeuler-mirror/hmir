bf118089a7c962f2b3f70aa1fdf194c620f55ac1
dev17101@linx-info.com <dev17101@linx-info.com>
2023-02-16 18:44:10 +0800

    加入系统页面其他对话框

--- hmir-frontend/src/views/system/index.vue
+++ hmir-frontend/src/views/system/index.vue
@@ -17,11 +17,11 @@
             <el-link type="primary">错误修复的更新可以使用</el-link>
           </div>
           <div>
-            <el-link type="primary">显示指印</el-link>
+            <el-link type="primary" @click="handleDialog('safe')">显示指印</el-link>
           </div>
-          <div><el-link type="primary" @click="handleComputerDialog">LINX</el-link></div>
-          <div><el-link type="primary">加入域</el-link></div>
-          <div><el-link type="primary">2023-02-16 10:35</el-link></div>
+          <div><el-link type="primary" @click="handleDialog('compuer')">LINX</el-link></div>
+          <div><el-link type="primary" @click="handleDialog('area')">加入域</el-link></div>
+          <div><el-link type="primary" @click="handleDialog('time')">2023-02-16 10:35</el-link></div>
           <div>
             <el-switch
                 v-model="data.value1"
@@ -30,7 +30,7 @@
               </el-switch>
           </div>
           <div>空</div>
-          <div><el-link type="primary">启用保存的指标</el-link></div>
+          <div><el-link type="primary" @click="handleDialog('save')">启用保存的指标</el-link></div>
         </div>
     </div>
     <div class="charts">
@@ -39,17 +39,66 @@
       <div></div>
       <div></div>
     </div>
+      <!-- 安全Shell密钥的对话框 -->
+       <el-dialog
+      title="主机 SSH 密钥指纹"
+      v-model="data.safeDialog"
+      width="30%"
+      :before-close="handleClose">
+      <span>安全Shell密钥的对话框</span>
+      <template #footer>
+        <el-button @click="data.safeDialog = false">取 消</el-button>
+        <el-button type="primary" @click="data.safeDialog = false">确 定</el-button>
+      </template>
+      </el-dialog>
+      <!-- 主机名的对话框 -->
       <el-dialog
-      title="提示"
+      title="修改主机名"
       v-model="data.dialogVisible"
       width="30%"
       :before-close="handleClose">
-      <span>这是一段信息</span>
+      <span>这是主机名对话框</span>
       <template #footer>
         <el-button @click="data.dialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="data.dialogVisible = false">确 定</el-button>
       </template>
-    </el-dialog>
+      </el-dialog>
+      <!-- 域的对话框 -->
+      <el-dialog
+      title="安装软件"
+      v-model="data.areaDialog"
+      width="30%"
+      :before-close="handleClose">
+      <span>这是域的对话框</span>
+      <template #footer>
+        <el-button @click="data.areaDialog = false">取 消</el-button>
+        <el-button type="primary" @click="data.areaDialog = false">确 定</el-button>
+      </template>
+      </el-dialog>
+      <!-- 系统时间对话框 -->
+       <el-dialog
+      title="修改系统时间"
+      v-model="data.timeDialog"
+      width="30%"
+      :before-close="handleClose">
+      <span>系统时间对话框</span>
+      <template #footer>
+        <el-button @click="data.timeDialog = false">取 消</el-button>
+        <el-button type="primary" @click="data.timeDialog = false">确 定</el-button>
+      </template>
+      </el-dialog>
+      <!-- 启用保存的指标对话框 -->
+       <el-dialog
+      title="安装软件"
+      v-model="data.saveDialog"
+      width="30%"
+      :before-close="handleClose">
+      <span>启用保存的指标对话框</span>
+      <template #footer>
+        <el-button @click="data.saveDialog = false">取 消</el-button>
+        <el-button type="primary" @click="data.saveDialog = false">确 定</el-button>
+      </template>
+      </el-dialog>
   </div>
   <hardwareDetail v-show="data.hardwareShow" @hardwareDetailShow ="hardwareDetailShow"></hardwareDetail>
 </template>
@@ -61,15 +110,37 @@ const data = ref({
   contentShow: true,
   option: ['硬件', '资产标签', '机器编码', '操作系统', '', '安全Shell密钥', '主机名', '域', '系统时间', '电源选项', '性能配置集', ''],
   value1: true,
+  safeDialog: false,
   dialogVisible: false,
+  areaDialog: false,
+  timeDialog: false,
+  saveDialog: false,
   hardwareShow: false
 })
 const hardwareDetailShow = () => {
   data.value.contentShow = !data.value.contentShow
   data.value.hardwareShow = !data.value.hardwareShow
 }
-const handleComputerDialog = () => {
-  data.value.dialogVisible = true
+const handleDialog = (val:String) => {
+  switch (val) {
+    case 'safe':
+      data.value.safeDialog = !data.value.safeDialog
+      break
+    case 'compuer':
+      data.value.dialogVisible = !data.value.dialogVisible
+      break
+    case 'area':
+      data.value.areaDialog = !data.value.areaDialog
+      break
+    case 'time':
+      data.value.timeDialog = !data.value.timeDialog
+      break
+    case 'save':
+      data.value.saveDialog = !data.value.saveDialog
+      break
+    default:
+      break
+  }
 }
 </script>
 
