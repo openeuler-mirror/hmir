b0916251c04404bfad0e79baa0d66a2cdc4fefc6
dev17101@linx-info.com <dev17101@linx-info.com>
2023-02-20 18:47:22 +0800

    仪表盘echarts引入

--- hmir-frontend/src/views/ceph/components/dashBoard/index.vue
+++ hmir-frontend/src/views/ceph/components/dashBoard/index.vue
@@ -3,39 +3,230 @@
     <div>
         <p>状态</p>
        <div class="status">
-        <div>集群状态</div>
-        <div>主机</div>
-        <div>Monitor</div>
-        <div>OSD</div>
-        <div>Manigers</div>
-        <div>对象网关</div>
-        <div>metaData 服务器</div>
-        <div>iSCSI网关</div>
+        <div class="status-box" v-for="(e,i) in data.statusData" :key="i">
+            <Status :statusData= data.statusData[i]></Status>
+        </div>
        </div>
     </div>
     <div>
         <p>容量</p>
         <div class="capacity">
-            <div>基本容量</div>
-            <div>对象树</div>
-            <div>PG状态</div>
-            <div>储存池</div>
+            <!-- <div class="capacity-box">基本容量</div> -->
+            <div class="capacity-box" v-for="(e,i) in 4" :key="i">
+                <Echarts :chartData = data.chartData[i] ></Echarts>
+            </div>
+            <!-- <div class="capacity-box">对象树</div>
+            <div class="capacity-box">PG状态</div>
+            <div class="capacity-box">储存池</div> -->
         </div>
     </div>
     <div>
         <p>性能</p>
         <div class="performance">
-            <div>客户端读写</div>
-            <div>客户端吞吐量</div>
-            <div>恢复复吞吐量</div>
-            <div>Scrubling</div>
+            <div class="performance-box">客户端读写</div>
+            <div class="performance-box">客户端吞吐量</div>
+            <div class="performance-box">恢复复吞吐量</div>
+            <div class="performance-box">Scrubling</div>
         </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { ref } from 'vue'
+import Status from './status.vue'
+import Echarts from './echarts.vue'
+const data = ref({
+  statusData: [
+    { name: '集群状态', content: 'HEALTY_OK' },
+    { name: '主机', content: 1 },
+    { name: 'Monitor', content: 1 },
+    { name: 'OSD', content: 1 },
+    { name: 'Manigers', content: 1 },
+    { name: '对象网关', content: 1 },
+    { name: 'metaData 服务器', content: 1 },
+    { name: 'iSCSI网关', content: 1 }
+  ],
+  chartData: [
+    {
+      id: 'chart1',
+      title: { text: '基本容量' },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        top: '5%',
+        left: 'center'
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: ['40%', '70%'],
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: '#fff',
+            borderWidth: 2
+          },
+          label: {
+            show: false,
+            position: 'center'
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: 40,
+              fontWeight: 'bold'
+            }
+          },
+          labelLine: {
+            show: false
+          },
+          data: [
+            { value: 100, name: 'Healthy' },
+            { value: 10, name: 'Misplaced' },
+            { value: 20, name: 'Degraed' },
+            { value: 30, name: 'Unfound' }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'chart2',
+      title: { text: '对象树' },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        top: '5%',
+        left: 'center'
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: ['40%', '70%'],
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: '#fff',
+            borderWidth: 2
+          },
+          label: {
+            show: false,
+            position: 'center'
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: 40,
+              fontWeight: 'bold'
+            }
+          },
+          labelLine: {
+            show: false
+          },
+          data: [
+            { value: 100, name: 'Healthy' },
+            { value: 10, name: 'Misplaced' },
+            { value: 20, name: 'Degraed' },
+            { value: 30, name: 'Unfound' }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'chart3',
+      title: { text: 'PG状态' },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        top: '5%',
+        left: 'center'
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: ['40%', '70%'],
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: '#fff',
+            borderWidth: 2
+          },
+          label: {
+            show: false,
+            position: 'center'
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: 40,
+              fontWeight: 'bold'
+            }
+          },
+          labelLine: {
+            show: false
+          },
+          data: [
+            { value: 100, name: 'Healthy' },
+            { value: 10, name: 'Misplaced' },
+            { value: 20, name: 'Degraed' },
+            { value: 30, name: 'Unfound' }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'chart4',
+      title: { text: '客户端吞吐量' },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        top: '5%',
+        left: 'center'
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: ['40%', '70%'],
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: '#fff',
+            borderWidth: 2
+          },
+          label: {
+            show: false,
+            position: 'center'
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: 40,
+              fontWeight: 'bold'
+            }
+          },
+          labelLine: {
+            show: false
+          },
+          data: [
+            { value: 100, name: 'Healthy' },
+            { value: 10, name: 'Misplaced' },
+            { value: 20, name: 'Degraed' },
+            { value: 30, name: 'Unfound' }
+          ]
+        }
+      ]
+    }
+  ]
+})
+// console.log(data.value.ids)
 </script>
 
 <style lang="scss" scoped>
@@ -43,8 +234,8 @@
     display: flex;
     justify-content: center;
     flex-wrap:wrap;
-    div{
-        border-radius: 4px;
+    .status-box{
+         border-radius: 4px;
         background: #f5f5f5;
         width: 24%;
         height: 108px;
@@ -55,26 +246,26 @@
 .capacity{
     display: flex;
      flex-wrap:nowrap;
-    div{
-        width: 24%;
+     .capacity-box{
+         width: 24%;
          border-radius: 4px;
          height: 200px;
          margin-right:1% ;
         margin-bottom: 1%;
          background: #f5f5f5 ;
-    }
+     }
 }
 .performance{
     display: flex;
      flex-wrap:nowrap;
-    div{
+     .performance-box{
         width: 24%;
          border-radius: 4px;
          height: 200px;
          background: #f5f5f5;
          margin-right:1% ;
          margin-bottom: 1%;
-    }
+     }
 }
 
 </style>
