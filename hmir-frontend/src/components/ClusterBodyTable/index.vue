fd6af5e27ab34f2d60e2a3a3b980a99edd6e59df
zhang_tianran <dev17101@linx-info.com>
2023-06-15 16:49:54 +0800

    构造表格列数据

--- hmir-frontend/src/components/ClusterBodyTable/index.vue
+++ hmir-frontend/src/components/ClusterBodyTable/index.vue
@@ -55,65 +55,6 @@ const emit = defineEmits({
 
 const clusterBodyTable = ref<InstanceType<typeof ElTable>>()
 
-// const tableData = [
-//   {
-//     date: '2016-05-03',
-//     name: 'Tom',
-//     state: 'California',
-//     city: 'San Francisco',
-//     address: '3650 21st St, San Francisco',
-//     zip: 'CA 94114'
-//   },
-//   {
-//     date: '2016-05-02',
-//     name: 'Tom',
-//     state: 'California',
-//     city: 'San Francisco',
-//     address: '3650 21st St, San Francisco',
-//     zip: 'CA 94114'
-//   },
-//   {
-//     date: '2016-05-04',
-//     name: 'Tom',
-//     state: 'California',
-//     city: 'San Francisco',
-//     address: '3650 21st St, San Francisco',
-//     zip: 'CA 94114'
-//   },
-//   {
-//     date: '2016-05-01',
-//     name: 'Tom',
-//     state: 'California',
-//     city: 'San Francisco',
-//     address: '3650 21st St, San Francisco',
-//     zip: 'CA 94114'
-//   },
-//   {
-//     date: '2016-05-08',
-//     name: 'Tom',
-//     state: 'California',
-//     city: 'San Francisco',
-//     address: '3650 21st St, San Francisco',
-//     zip: 'CA 94114'
-//   },
-//   {
-//     date: '2016-05-06',
-//     name: 'Tom',
-//     state: 'California',
-//     city: 'San Francisco',
-//     address: '3650 21st St, San Francisco',
-//     zip: 'CA 94114'
-//   },
-//   {
-//     date: '2016-05-07',
-//     name: 'Tom',
-//     state: 'California',
-//     city: 'San Francisco',
-//     address: '3650 21st St, San Francisco',
-//     zip: 'CA 94114'
-//   }
-// ]
-
 const rowClick = (row: Object, column: any) => {
   clusterBodyTable.value!.setCurrentRow(row)
   console.log(row, column)
--- hmir-frontend/src/views/cluster/hosts/index.vue
+++ hmir-frontend/src/views/cluster/hosts/index.vue
@@ -2,7 +2,7 @@
  * @Author: zhang_tianran
  * @Date: 2023-06-14 09:47:34
  * @LastEditors: zhang_tianran
- * @LastEditTime: 2023-06-14 17:54:35
+ * @LastEditTime: 2023-06-15 16:45:46
  * @Description:
 -->
 <template>
@@ -10,9 +10,9 @@
     <template v-slot:breadcrumbBody>
       <el-tabs type="card">
         <el-tab-pane label="Hosts List">
-          <ClusterBodyTable>
+          <ClusterBodyTable @selectRowData="rowClick" :tableData="tableData" :tableColumn="tableColumn">
             <template v-slot:tableTitleLeft>
-              <hostTableTitleLeft></hostTableTitleLeft>
+              <hostTableTitleLeft :selectRow="selectRow"></hostTableTitleLeft>
             </template>
             <template v-slot:tableTitleRight>
               <ClusterTableTitleRight></ClusterTableTitleRight>
@@ -38,12 +38,68 @@ const store = hostsProcStore()
 
 const breadcrumbTitle = ref()
 
+const selectRow = ref<Object>({})
+
+const rowClick = (row: Object) => {
+  selectRow.value = row
+}
+
+const tableData = ref([])
+
+const tableColumn = ref([{
+  label: 'Hostname',
+  prop: 'hostname',
+  sortable: true
+}, {
+  label: 'Service Instances',
+  prop: 'service',
+  sortable: true
+}, {
+  label: 'Labels',
+  prop: 'labels',
+  sortable: true
+}, {
+  label: 'Status',
+  prop: 'status',
+  sortable: true
+}, {
+  label: 'Model',
+  prop: 'model',
+  sortable: true
+}, {
+  label: 'CPUs',
+  prop: 'cpus',
+  sortable: true
+}, {
+  label: 'Cores',
+  prop: 'cores',
+  sortable: true
+}, {
+  label: 'Total Memory',
+  prop: 'totalMemory',
+  sortable: true
+}, {
+  label: 'Raw Capacity',
+  prop: 'capacity',
+  sortable: true
+}, {
+  label: 'HDDs',
+  prop: 'hdds',
+  sortable: true
+}, {
+  label: 'Flash',
+  prop: 'flash',
+  sortable: true
+}, {
+  label: 'NICs',
+  prop: 'nics',
+  sortable: true
+}])
+
 onMounted(() => {
   breadcrumbTitle.value = store.get_defaultTitle(['Hosts'])
 })
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
