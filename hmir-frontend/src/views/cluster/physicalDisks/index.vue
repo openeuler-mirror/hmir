92b86fde79b560a3f0d39babd3b2aead4aa9d206
zhang_tianran <dev17101@linx-info.com>
2023-06-30 10:20:29 +0800

    完成集群物理磁盘相关页面

---
+++ hmir-frontend/src/views/cluster/physicalDisks/index.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Author: zhang_tianran
+ * @Date: 2023-06-30 10:05:05
+ * @LastEditors: zhang_tianran
+ * @LastEditTime: 2023-06-30 10:17:55
+ * @Description:
+-->
+<template>
+  <breadcrumb :breadcrumb="breadcrumbTitle">
+    <template v-slot:breadcrumbBody>
+      <PhysicalDisks :physicalDiskData="tableData"></PhysicalDisks>
+    </template>
+  </breadcrumb>
+
+</template>
+
+<script setup lang="ts">
+import PhysicalDisks from '@/views/cluster/components/PhysicalDisks.vue'
+import breadcrumb from '@/components/ClusterHeader/index.vue'
+import { onMounted, ref } from 'vue'
+import { hostsProcStore } from '@/store/modules/cluster/host'
+
+// 引入store仓库
+const store = hostsProcStore()
+
+const breadcrumbTitle = ref()
+
+const tableData = ref([{
+  devicePath:'Device path',
+  type:'Type',
+  available:'Available',
+  vendor:'Vendor',
+  model:'Model',
+  size:'Size',
+  osds:'OSDs'
+}])
+
+onMounted(() => {
+  breadcrumbTitle.value = store.get_defaultTitle(['Physical Disks'])
+})
+</script>
+
+<style lang="scss" scoped></style>
