1bc10f0f6d817ad265279ad9c515840d6193598e
zhang_tianran <dev17101@linx-info.com>
2023-07-04 16:23:27 +0800

    封装OSDs页面集群配置OSD Recovery Priority弹窗组件

---
+++ hmir-frontend/src/views/cluster/OSDs/components/recoveryPriority.vue
@@ -0,0 +1,51 @@
+<!--
+ * @Author: zhang_tianran
+ * @Date: 2023-07-04 16:17:40
+ * @LastEditors: zhang_tianran
+ * @LastEditTime: 2023-07-04 16:21:49
+ * @Description:
+-->
+<template>
+  <el-dialog :model-value="dialogVisible" title="OSD Recovery Priority" width="30%" @closed="cancel">
+    <span>This is a message</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="cancel">Cancel</el-button>
+        <el-button type="primary" @click="submit">
+          Confirm
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+// import { ref } from 'vue'
+
+defineProps({
+  dialogVisible: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emit = defineEmits({
+  // eslint-disable-next-line no-unused-vars
+  cancel: (_type: string, _data: boolean) => true
+})
+
+const cancel = () => {
+  emit('cancel', 'flagsDialog', false)
+}
+
+const submit = () => {
+  emit('cancel', 'flagsDialog', false)
+}
+
+</script>
+
+<style lang="scss" scoped>
+.dialog-footer button:first-child {
+  margin-right: 10px;
+}
+</style>
