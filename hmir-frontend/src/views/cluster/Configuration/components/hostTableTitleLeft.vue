3dcc51a417ce736f4a708aaa970a3c28700be3ff
zhang_tianran <dev17101@linx-info.com>
2023-08-04 16:21:32 +0800

    增加集群配置左侧功能组件

---
+++ hmir-frontend/src/views/cluster/Configuration/components/hostTableTitleLeft.vue
@@ -0,0 +1,79 @@
+<!--
+ * @Author: zhang_tianran
+ * @Date: 2023-06-14 15:29:38
+ * @LastEditors: zhang_tianran
+ * @LastEditTime: 2023-07-31 10:32:04
+ * @Description:
+-->
+<template>
+  <ClusterTableTitleLeft :dropdownList="dropdownArray" @handleClick="handleClick"></ClusterTableTitleLeft>
+  <hostDialog :dialogVisible="dialogVisible" :hostType="hostType" :selectRow="selectRow" @cancel="dialogChange"></hostDialog>
+</template>
+
+<script setup lang="ts">
+import ClusterTableTitleLeft from '@/components/ClusterTableTitleLeft/index.vue'
+import hostDialog from './hostDialog.vue'
+import { computed, ref, watch } from 'vue'
+
+const props = defineProps({
+  selectRow: {
+    type: Object,
+    default() {
+      return {}
+    }
+  }
+})
+
+const selectRow = computed(() => {
+  return props.selectRow
+})
+
+const dropdownArray = ref([
+  { command: 'add', value: 'Add', disabled: false },
+  { command: 'edit', value: 'Edit', disabled: true },
+  { command: 'stopDrain', value: 'Stop Drain', disabled: true },
+  { command: 'remove', value: 'Remove', disabled: true },
+  { command: 'enterMaintenance', value: 'Enter Maintenance', disabled: true }
+])
+
+const dialogVisible = ref(false)
+
+const hostType = ref('')
+
+const handleClick = (dropdownText: string) => {
+  hostType.value = dropdownText
+  switch (dropdownText) {
+    case 'add':
+    case 'edit':
+      openDialog()
+      break
+    default:
+      break
+  }
+}
+
+watch(selectRow, (value) => {
+  if (Object.keys(value).length === 0) {
+    dropdownArray.value.forEach(item => {
+      if (item.command !== 'add') {
+        item.disabled = true
+      }
+    })
+  } else {
+    dropdownArray.value.forEach(item => {
+      item.disabled = false
+    })
+  }
+})
+
+const dialogChange = (value: boolean) => {
+  dialogVisible.value = value
+}
+
+const openDialog = () => {
+  dialogChange(true)
+}
+
+</script>
+
+<style lang="scss" scoped></style>
