5bec7eeb26680d769bb1addfa52792d17586b21c
Z&N <dev17101@linx-info.com>
2024-10-29 16:36:03 +0800

    封装ResizeObserver函数，用于实时更新当前系统的高度，便于调整页面布局

--- hmir-frontend/src/App.vue
+++ hmir-frontend/src/App.vue
@@ -1,6 +1,14 @@
+<!--
+ * @Author: Z&N dev17101@linx-info.com
+ * @Date: 2024-10-23 09:38:36
+ * @LastEditors: Z&N dev17101@linx-info.com
+ * @LastEditTime: 2024-10-29 16:31:48
+ * @FilePath: /hmir-frontend/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <windowHeader></windowHeader>
-  <div class="windowBody">
+  <div class="windowBody" ref="appMainRef">
     <el-config-provider :locale="useAppStoreHook().locale === 'zh_CN' ? zhLocale : enLocale">
       <router-view />
     </el-config-provider>
@@ -15,6 +23,27 @@ import windowHeader from '@/views/windowHeader/index.vue'
 import enLocale from 'element-plus/es/locale/lang/en'
 import zhLocale from 'element-plus/es/locale/lang/zh-cn'
 import { useAppStoreHook } from '@/store/modules/app' // store存放语言配置
+import { ref, onMounted, nextTick, onBeforeUnmount } from 'vue'
+import { addResizeObserver } from './utils/utils'
+import { useHeightStoreHook } from '@/store/modules/appMainHeight'
+
+const appMainRef = ref()
+
+const store = ref(useHeightStoreHook())
+
+const resizeObserver = ref()
+
+onMounted(() => {
+  nextTick(() => {
+    resizeObserver.value = addResizeObserver(appMainRef.value, (entry) => {
+      store.value.UPDATE_HEIGHT(entry.target?.offsetHeight || 650)
+    })
+  })
+})
+
+onBeforeUnmount(() => {
+  resizeObserver.value?.disconnect()
+})
 </script>
 
 <style scoped>
