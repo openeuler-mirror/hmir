55f5e699b5ff29f25f143ed236ff2e6a0b388956
Z&N <dev17101@linx-info.com>
2024-11-05 14:29:28 +0800

    封装通用搜索组件，支持更丰富的搜索功能，增强用户体验

---
+++ hmir-frontend/src/components/FormSearch/index.vue
@@ -0,0 +1,201 @@
+<!--
+ * Copyright (C) 2016-2021 - Beijing Linx Software Corp.
+-->
+<template>
+  <div class="search-box">
+    <el-form inline>
+      <ComFlexSpace>
+        <el-form-item :label="$t('queryFields')">
+          <el-select
+            v-model="searchInfo.searchLabel"
+            :placeholder="$t('pleaseSelect')"
+            clearable
+            :style="{ width: inputWidth }"
+            @change="searchLabelChange"
+          >
+            <el-option
+              v-for="item in searchLabelOptions"
+              :key="item.value"
+              :label="$t(item.label)"
+              :value="item.value"
+            />
+          </el-select>
+        </el-form-item>
+
+        <el-form-item :label="$t('queryMethod')">
+          <el-select
+            v-model="searchInfo.searchType"
+            :placeholder="$t('pleaseSelect')"
+            :style="{ width: inputWidth }"
+          >
+            <el-option
+              v-for="item in searchTypeDataOptions"
+              v-show="item.show"
+              :key="item.value"
+              :label="$t(item.label)"
+              :value="item.value"
+            />
+          </el-select>
+        </el-form-item>
+
+        <div>
+          <el-form-item :label="$t('queryContent')">
+            <el-input
+              v-if="searchValueOptionsShowType === 'input'"
+              v-model="searchInfo.searchInputName"
+              style="width: 220px"
+              :placeholder="$t('pleaseInputContent')"
+              :prefix-icon="Search"
+              @keyup.enter.stop="searchList"
+            />
+            <el-select
+              v-if="searchValueOptionsShowType === 'select'"
+              v-model="searchInfo.searchInputName"
+              :placeholder="$t('pleaseSelect')"
+              style="width: 220px"
+            >
+              <el-option
+                v-for="item in searchValueOptionSelect"
+                :key="item.value"
+                :label="$t(item.label)"
+                :value="item.value"
+                :disabled="item.disabled"
+              />
+            </el-select>
+            <el-tree-select
+              v-if="searchValueOptionsShowType === 'treeSelect'"
+              v-model="searchInfo.searchInputName"
+              filterable
+              default-expand-all
+              :data="searchValueOptionSelect"
+              style="width: 220px"
+              :placeholder="$t('pleaseSelect')"
+              :node-key="treeNodeKey"
+              :props="{ value: treeNodeKey, label: 'name', children: 'children', disabled: disabledTreeNode }"
+            >
+              <template #default="{ data }">
+                <template v-if="data.name.length < 13">
+                  <span> {{ data.name }}</span>
+                </template>
+                <template v-else>
+                  <el-tooltip
+                    class="item"
+                    effect="dark"
+                    :content="data.name"
+                    :enterable="false"
+                    placement="top"
+                  >
+                    <span> {{ data }}</span>
+                  </el-tooltip>
+                </template>
+              </template>
+            </el-tree-select>
+          </el-form-item>
+
+          <el-form-item style="margin-left: 16px;">
+            <el-button
+              id="form_btn_search"
+              type="primary"
+              @click="searchList"
+            >
+              {{ $t('search') }}
+            </el-button>
+          </el-form-item>
+        </div>
+      </ComFlexSpace>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import ComFlexSpace from '@/components/ComFlexSpace/index.vue'
+import { Search } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+
+defineProps({
+  inputWidth: {
+    type: String,
+    default: '140px'
+  },
+  searchLabelOptions: {
+    type: Array,
+    default() {
+      return []
+    }
+  },
+  // 不建议直接修改父组件传过来的值
+  searchTypeOptions: {
+    type: Array,
+    default() {
+      return [
+        {
+          label: 'fuzzyMatching',
+          value: 0,
+          show: true
+        },
+        {
+          label: 'accurateMatching',
+          value: 1,
+          show: true
+        },
+        {
+          label: 'unequalMatching',
+          value: 2,
+          show: true
+        },
+        {
+          label: 'greaterThanEqual',
+          value: 5,
+          show: false
+        },
+        {
+          label: 'lessThanEqual',
+          value: 6,
+          show: false
+        }
+      ]
+    }
+  },
+  searchValueOptions: {
+    type: Object,
+    default() {
+      return {
+        default: {}
+      }
+    }
+  },
+  treeNodeKey: {
+    type: String,
+    default: 'distinctId'
+  },
+  disabledTreeNode: {
+    type: Function,
+    default: () => false
+  }
+})
+
+const emits = defineEmits(['search', 'searchLabelChange'])
+
+const searchInfo = ref({
+  searchLabel: '',
+  searchType: 0,
+  searchInputName: ''
+})
+
+const searchValueOptionSelect = ref([])
+
+const searchTypeDataOptions = ref([])
+
+const searchValueOptionsShowType = ref('input')
+
+function searchList() {
+  emits('search', searchInfo.value)
+}
+
+function searchLabelChange(value) {
+  emits('searchLabelChange', value)
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
