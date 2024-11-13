2c985a86e9c27ca3381ee21e62dad724f24ff989
Z&N <dev17101@linx-info.com>
2024-11-13 15:56:49 +0800

    实现通过调用astexplorer内部方法实现抽象语法树的解析

--- hmir-frontend/src/views/parsers/index.vue
+++ hmir-frontend/src/views/parsers/index.vue
@@ -1,12 +1,14 @@
+<!-- eslint-disable no-underscore-dangle -->
 <!--
  * @Author: Z&N dev17101@linx-info.com
  * @Date: 2024-11-13 14:34:32
  * @LastEditors: Z&N
- * @LastEditTime: 2024-11-13 14:54:20
+ * @LastEditTime: 2024-11-13 15:50:10
  * @FilePath: /hmir-frontend/src/views/parsers/index.vue
  * @Description:
 -->
 <template>
+  <div>{{ astTree }}</div>
   <div id="parsers">
     <iframe
       ref="reference"
@@ -16,14 +18,46 @@
 </template>
 
 <script setup>
-import { ref, onMounted, nextTick } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const reference = ref()
 
+const parsersInfo = ref({})
+
+function parse(parser, code, parserSettings) {
+  if (!parser._promise) {
+    parser._promise = new Promise(parser.loadParser)
+  }
+  return parser._promise.then(
+    realParser => parser.parse(
+      realParser,
+      code,
+      parserSettings || parser.getDefaultOptions()
+    )
+  )
+}
+
+const astTree = ref()
+
 onMounted(() => {
-  nextTick(() => {
+  setTimeout(() => {
+    const parsersContentWindow = reference.value.contentWindow
+
     console.log(reference.value.contentWindow)
-  })
+
+    console.log(parsersContentWindow.$initParsers)
+
+    parsersInfo.value = parsersContentWindow.$initParsers()
+
+    const babylon7 = parsersInfo.value.parserByID.babylon7
+
+    parse(babylon7, 'console.log(dwa)', babylon7.getDefaultOptions()).then(
+      ast => {
+        astTree.value = JSON.stringify(ast)
+        console.log(ast)
+      }
+    )
+  }, 1000)
 })
 </script>
 
