419eb2a6edaf279c224014c533e71d0f30fd9119
Z&N <dev17101@linx-info.com>
2024-11-13 15:25:57 +0800

    astexplorer功能集成parsers页

--- hmir-frontend/src/views/parsers/index.vue
+++ hmir-frontend/src/views/parsers/index.vue
@@ -2,18 +2,38 @@
  * @Author: Z&N dev17101@linx-info.com
  * @Date: 2024-11-13 14:34:32
  * @LastEditors: Z&N
- * @LastEditTime: 2024-11-13 14:35:27
+ * @LastEditTime: 2024-11-13 14:54:20
  * @FilePath: /hmir-frontend/src/views/parsers/index.vue
  * @Description:
 -->
 <template>
-  <div />
+  <div id="parsers">
+    <iframe
+      ref="reference"
+      src="/out/index.html"
+    />
+  </div>
 </template>
 
 <script setup>
+import { ref, onMounted, nextTick } from 'vue'
 
+const reference = ref()
+
+onMounted(() => {
+  nextTick(() => {
+    console.log(reference.value.contentWindow)
+  })
+})
 </script>
 
 <style lang="scss" scoped>
-
+#parsers {
+    display: none;
+    height: 100%;
+    iframe {
+        height: 100%;
+        width: 100%;
+    }
+}
 </style>
