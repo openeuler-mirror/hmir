01d78c8738556ed544566acf18f74c9f2fcfdf99
Z&N <dev17101@linx-info.com>
2024-12-04 11:28:56 +0800

    通用表格组件封装，prop传参参数规范性修改

--- hmir-frontend/src/components/BecommonTable/index.vue
+++ hmir-frontend/src/components/BecommonTable/index.vue
@@ -2,106 +2,103 @@
  * @Author: zhang_tianran
  * @Date: 2023-06-14 14:03:21
  * @LastEditors: Z&N
- * @LastEditTime: 2024-12-04 10:15:46
+ * @LastEditTime: 2024-12-04 11:26:19
  * @Description:
 -->
 <template>
-  <div class="tableTitle">
-    <el-row>
-      <el-col :span="8">
-        <div class="grid-content">
-          <slot name="tableTitleLeft" />
-        </div>
-      </el-col>
-      <el-col :span="16">
-        <div class="grid-content tableTitleRight">
-          <slot name="tableTitleRight" />
-        </div>
-      </el-col>
-    </el-row>
-  </div>
-  <el-table
-    ref="clusterBodyTable"
-    :data="tableData"
-    :row-key="rowKey"
-    border
-    style="width: 100%"
-    :highlight-current-row="highlightCurrentRow"
-    @row-click="rowClick"
-    @expand-change="expandChange"
-  >
-    <el-table-column
-      v-if="expandShow"
-      type="expand"
-    >
-      <template #default="{ row }">
-        <slot
-          name="expand"
-          :row="row"
-        />
+  <div>
+    <BecommonTableHead ref="BecommonTableHeadRef">
+      <template #tableTitleLeft>
+        <slot name="tableTitleLeft" />
       </template>
-    </el-table-column>
-    <el-table-column
-      v-for="column in filteredTableColumn"
-      :key="column.prop"
-      :label="t(column.label)"
-      :prop="column.prop"
-      :sortable="column.sortable"
-      :show-overflow-tooltip="column.showTooltip"
-      :width="column.width"
+      <template #tableTitleRight>
+        <slot name="tableTitleRight" />
+      </template>
+    </BecommonTableHead>
+    <el-table
+      ref="clusterBodyTable"
+      :data="tableData"
+      :row-key="rowKey"
+      border
+      style="width: 100%"
+      :highlight-current-row="highlightCurrentRow"
+      @row-click="rowClick"
+      @expand-change="expandChange"
     >
-      <template #default="{ row }">
-        <el-progress
-          v-if="column.type === 'progress'"
-          :stroke-width="15"
-          :percentage="row[column.prop]"
-        />
-        <el-link
-          v-else-if="column.type === 'link'"
-          type="primary"
-          @click="lineClick(row, column)"
-        >
-          {{ tableValue(row, column) }}
-        </el-link>
-        <template v-else-if="column.type === 'linkList'">
-          <div
-            v-for="(linkItem, index) in row[column.prop]"
-            :key="linkItem"
+      <el-table-column
+        v-if="expandShow"
+        type="expand"
+      >
+        <template #default="{ row }">
+          <slot
+            name="expand"
+            :row="row"
+          />
+        </template>
+      </el-table-column>
+      <el-table-column
+        v-for="column in filteredTableColumn"
+        :key="column.prop"
+        :label="t(column.label)"
+        :prop="column.prop"
+        :sortable="column.sortable"
+        :show-overflow-tooltip="column.showTooltip"
+        :width="column.width"
+      >
+        <template #default="{ row }">
+          <el-progress
+            v-if="column.type === 'progress'"
+            :stroke-width="15"
+            :percentage="row[column.prop]"
+          />
+          <el-link
+            v-else-if="column.type === 'link'"
+            type="primary"
+            @click="lineClick(row, column)"
           >
-            <el-link
-              type="primary"
-              @click="lineClick(row, column, linkItem)"
+            {{ tableValue(row, column) }}
+          </el-link>
+          <template v-else-if="column.type === 'linkList'">
+            <div
+              v-for="(linkItem, index) in row[column.prop]"
+              :key="linkItem"
             >
-              {{ tableLinkListValue(row, column, linkItem) }}
-            </el-link>
-            <el-divider
-              v-if="index !== row[column.prop].length - 1"
-              direction="vertical"
-            />
+              <el-link
+                type="primary"
+                @click="lineClick(row, column, linkItem)"
+              >
+                {{ tableLinkListValue(row, column, linkItem) }}
+              </el-link>
+              <el-divider
+                v-if="index !== row[column.prop].length - 1"
+                direction="vertical"
+              />
+            </div>
+          </template>
+          <div v-else>
+            {{ tableValue(row, column) }}
           </div>
         </template>
-        <div v-else>
-          {{ tableValue(row, column) }}
-        </div>
-      </template>
-    </el-table-column>
-  </el-table>
-  <div v-if="paginationShow">
-    <slot name="Pagination">
-      <Pagination
-        :total="total"
-        :disabled="paginationDisabled"
-        :background="background"
-        :small="small"
-        :hide-on-single-page="hideOnSinglePage"
-        @handleSizeChange="handleSizeChange"
-        @handleCurrentChange="handleCurrentChange"
-      />
-    </slot>
+      </el-table-column>
+    </el-table>
+    <div v-if="paginationShow">
+      <slot name="Pagination">
+        <Pagination
+          :total="total"
+          :disabled="paginationDisabled"
+          :background="background"
+          :small="small"
+          :hide-on-single-page="hideOnSinglePage"
+          @handleSizeChange="handleSizeChange"
+          @handleCurrentChange="handleCurrentChange"
+        />
+      </slot>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import BecommonTableHead from './subview/BecommonTableHead.vue'
 import Pagination from '@/components/Pagination/index.vue'
 import { ref, computed } from 'vue'
 import type { ElTable } from 'element-plus'
@@ -221,22 +218,5 @@ const handleCurrentChange = (val: number) => {
 </script>
 
 <style lang="scss" scoped>
-.tableTitle {
-  display: flex;
-  justify-content: space-between;
 
-  .tableTitleRight {
-    float: right;
-  }
-}
-
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
-
-.el-row {
-  width: 100%;
-  padding-bottom: 10px;
-}
 </style>
---
+++ hmir-frontend/src/components/BecommonTable/subview/BecommonTableHead.vue
@@ -0,0 +1,40 @@
+<!--
+ * @Author: Z&N dev17101@linx-info.com
+ * @Date: 2024-12-04 09:23:26
+ * @LastEditors: Z&N
+ * @LastEditTime: 2024-12-04 10:33:02
+ * @FilePath: /hmir-frontend/src/components/BecommonTable/subview/BecommonTableHead.vue
+ * @Description:
+-->
+<template>
+  <el-row
+    ref="BecommonTableHeadRef"
+    class="BecommonTableHead"
+  >
+    <el-col
+      :span="12"
+      class="leftElcol"
+    >
+      <slot name="tableTitleLeft" />
+    </el-col>
+    <el-col
+      :span="12"
+      class="rightElcol"
+    >
+      <slot name="tableTitleRight" />
+    </el-col>
+  </el-row>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const BecommonTableHeadRef = ref()
+
+</script>
+
+<style lang="scss" scoped>
+.rightElcol {
+  display: inline-block;
+}
+</style>
