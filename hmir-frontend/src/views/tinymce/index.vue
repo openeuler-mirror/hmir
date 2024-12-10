d5edfcc6c5a06cd9eb6aea60f860cc5acb6444d0
Z&N <dev17101@linx-info.com>
2024-12-10 18:37:10 +0800

    在tauri中对生成的的文件进行保存

--- hmir-frontend/src/views/tinymce/index.vue
+++ hmir-frontend/src/views/tinymce/index.vue
@@ -2,7 +2,7 @@
  * @Author: Z&N dev17101@linx-info.com
  * @Date: 2024-12-10 11:04:51
  * @LastEditors: Z&N
- * @LastEditTime: 2024-12-10 17:30:35
+ * @LastEditTime: 2024-12-10 18:22:32
  * @FilePath: /hmir-frontend/src/views/tinymce/index.vue
  * @Description:
 -->
@@ -17,7 +17,11 @@
 import Tinymce from '@/components/Tinymce/index.vue'
 import { ref, watch } from 'vue'
 import { asBlob } from 'html-docx-js-typescript'
+// eslint-disable-next-line no-unused-vars
 import saveAs from 'file-saver'
+import { writeBinaryFile } from '@tauri-apps/api/fs'
+
+import { path, dialog } from '@tauri-apps/api'
 
 const content = ref('')
 
@@ -38,6 +42,43 @@ async function goDocxFile() {
       </body>
       </html>`)
 
-  saveAs(converted, 'test.docx')
+  blobToUint8Array(converted).then(async(uint8Array) => {
+    try {
+      console.log(uint8Array)
+      const basePath = await path.downloadDir()
+
+      let selPath = await dialog.save({
+
+        defaultPath: basePath
+
+      })
+
+      selPath = selPath.replace(/Untitled$/, '')
+
+      writeBinaryFile({ contents: uint8Array, path: `${selPath}` })
+    } catch (error) {
+      console.error(error)
+    }
+  }).catch(error => {
+    console.error('Error converting blob to Uint8Array:', error)
+  })
+
+  // saveAs(converted, 'test.docx')
 }
+
+async function blobToUint8Array(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+
+    reader.onloadend = () => {
+      if (reader.error) {
+        reject(reader.error)
+      } else {
+        resolve(new Uint8Array(reader.result))
+      }
+    }
+    reader.readAsArrayBuffer(blob)
+  })
+}
+
 </script>
