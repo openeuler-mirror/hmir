9ee1d2cae6d5b10910ba0f773364c9ce8ca91c72
Z&N <dev17101@linx-info.com>
2024-12-10 15:38:40 +0800

    组件参数绑定使用，实现对输入内容的双向监听

---
+++ hmir-frontend/src/views/tinymce/index.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: Z&N dev17101@linx-info.com
+ * @Date: 2024-12-10 11:04:51
+ * @LastEditors: Z&N
+ * @LastEditTime: 2024-12-10 15:05:27
+ * @FilePath: /hmir-frontend/src/views/tinymce/index.vue
+ * @Description:
+-->
+<template>
+  <Tinymce v-model="content" />
+</template>
+
+<script setup lang="ts">
+import Tinymce from '@/components/Tinymce/index.vue'
+import { ref, watch } from 'vue'
+
+const content = ref('')
+
+watch(content, (val) => {
+  console.log(val)
+})
+
+</script>
