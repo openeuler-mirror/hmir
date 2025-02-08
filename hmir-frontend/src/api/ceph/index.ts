66534bf79f19215d9a4963b28c445f2034f6710a
Z&N <dev17101@linx-info.com>
2025-02-08 13:25:40 +0800

    配置项修改，取消对Cargo.lock文件修改的跟踪

--- hmir-frontend/src/api/ceph/index.ts
+++ hmir-frontend/src/api/ceph/index.ts
@@ -2,11 +2,11 @@
  * @Author: Z&N dev17101@linx-info.com
  * @Date: 2024-10-23 09:38:36
  * @LastEditors: Z&N
- * @LastEditTime: 2025-02-07 17:06:34
+ * @LastEditTime: 2025-02-08 13:09:09
  * @FilePath: /hmir-frontend/src/api/ceph/index.ts
  * @Description: 
  */
-import { invoke } from '@tauri-apps/api/core'
+import invoke from '@/utils/invokeTauri';
 
 //调用函数前的时间限制
 const timeout = 0;
